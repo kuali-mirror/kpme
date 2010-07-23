26d3b11c8f0096145d7a2ec4672eea7b5797068d
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-07-23 14:17:33 +0000

    updated name to be consistent
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@212 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/main/java/org/kuali/hr/time/salgroup/SalGroup.java
@@ -0,0 +1,78 @@
+package org.kuali.hr.time.salgroup;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.LinkedHashMap;
+
+import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
+
+public class SalGroup extends PersistableBusinessObjectBase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long salGroupId;
+	private String salGroup;
+	private Date effectiveDate;
+	private Timestamp timestamp;
+	private boolean active;
+	
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	protected LinkedHashMap toStringMapper() {
+		return null;
+	}
+
+
+	public Long getSalGroupId() {
+		return salGroupId;
+	}
+
+
+	public void setSalGroupId(Long salGroupId) {
+		this.salGroupId = salGroupId;
+	}
+
+
+	public String getSalGroup() {
+		return salGroup;
+	}
+
+
+	public void setSalGroup(String salGroup) {
+		this.salGroup = salGroup;
+	}
+
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+
+	public void setEffectiveDate(Date effectiveDate) {
+		this.effectiveDate = effectiveDate;
+	}
+
+
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
+
+
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+}
