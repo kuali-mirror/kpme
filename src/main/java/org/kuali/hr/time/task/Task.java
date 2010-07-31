98dda53f9c70c0bbd2afd9f676e045429ccec759
rhansen <rhansen@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-07-31 15:01:53 +0000

    Updated to reflect new data model
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@240 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/task/Task.java
+++ src/main/java/org/kuali/hr/time/task/Task.java
@@ -1,9 +1,7 @@
 package org.kuali.hr.time.task;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.LinkedHashMap;
-
 import org.kuali.rice.kns.bo.PersistableBusinessObjectBase;
 
 public class Task extends PersistableBusinessObjectBase {
@@ -13,75 +11,84 @@ public class Task extends PersistableBusinessObjectBase {
      */
     private static final long serialVersionUID = 1L;
     
-    private Long taskId;
-    private Long workAreaId;
-    private Date effectiveDate;
+    private long taskId;
+    private long workAreaId;
     private String description;
-    private String adminDescription;
-    private Timestamp timestamp;
-    private boolean active;
+    private String departmentId;
+    private String userPrincipalId;
+    private Date effectiveDate;
+    private String administrativeDescription;
     
-    @SuppressWarnings("unchecked")
-    @Override
-    protected LinkedHashMap toStringMapper() {
-	// TODO Auto-generated method stub
-	return null;
+    public long getTaskId()
+    {
+    	return taskId;
+    }
+    
+    public void setTaskId(long taskId)
+    {
+    	this.taskId = taskId;
+    }
+    
+    public long getWorkAreaId()
+    {
+    	return workAreaId;
+    }
+    
+    public void setWorkAreaId(long workAreaId){
+    	this.workAreaId = workAreaId;
+    }
+    
+    public String getDescription()
+    {
+    return description;	
+    }
+    
+    public void setDescription(String desc)
+    {
+    	this.description = desc;
+    }
+    
+    public String getDepartmentId()
+    {
+    	return departmentId;
+    }
+    
+    public void setDepartmentId(String deptId){
+    	this.departmentId = deptId;
+    }
+    
+    public String getUserPrincipalId(){
+    	return userPrincipalId;
+    }
+    
+    public void setUserPrincipalId(String pid)
+    {
+    	this.userPrincipalId = pid;
+    }
+    
+    public Date getEffectiveDate(){
+    	return effectiveDate;
+    }
+    
+    public void setEffectiveDate(Date ed)
+    {
+    	this.effectiveDate = ed;
+    }
+    
+    public String getAdministrativeDescription(){
+    	return administrativeDescription;
+    }
+    
+    public void setAdministrativeDescription(String addesc)
+    {
+    	this.administrativeDescription = addesc;
     }
 
-    public Long getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(Long taskId) {
-        this.taskId = taskId;
-    }
-
-    public Long getWorkAreaId() {
-        return workAreaId;
-    }
-
-    public void setWorkAreaId(Long workAreaId) {
-        this.workAreaId = workAreaId;
-    }
-
-    public Date getEffectiveDate() {
-        return effectiveDate;
-    }
-
-    public void setEffectiveDate(Date effectiveDate) {
-        this.effectiveDate = effectiveDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getAdminDescription() {
-        return adminDescription;
-    }
-
-    public void setAdminDescription(String adminDescription) {
-        this.adminDescription = adminDescription;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
+	@Override
+	protected LinkedHashMap toStringMapper() {
+		
+		return null;
+	}
+    
 
 }
