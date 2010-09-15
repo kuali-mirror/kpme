d1c5ec5965cffa874db58689f1307abfc79c9f90
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-09-15 20:18:03 +0000

    adding maintanable for assignment
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/datamodelchanges@389 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/main/java/org/kuali/hr/time/assignment/service/AssignmentMaintainableServiceImpl.java
@@ -0,0 +1,37 @@
+package org.kuali.hr.time.assignment.service;
+
+import org.kuali.hr.time.assignment.Assignment;
+import org.kuali.hr.time.assignment.AssignmentAccount;
+import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+
+public class AssignmentMaintainableServiceImpl extends KualiMaintainableImpl{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void saveBusinessObject() {
+		Assignment assignment = (Assignment)this.getBusinessObject();
+		Assignment oldAssignment = KNSServiceLocator.getBusinessObjectService().findBySinglePrimaryKey(Assignment.class, 
+									assignment.getTkAssignmentId());
+		if(oldAssignment != null){
+			oldAssignment.setActive(false);
+			KNSServiceLocator.getBusinessObjectService().save(oldAssignment);
+			for(AssignmentAccount oldAssignAccount : oldAssignment.getAssignmentAccounts()){
+				oldAssignAccount.setActive(false);
+				KNSServiceLocator.getBusinessObjectService().save(oldAssignAccount);
+			}
+		}
+		assignment.setTimestamp(null);
+		assignment.setTkAssignmentId(null);
+		KNSServiceLocator.getBusinessObjectService().save(assignment);
+		for(AssignmentAccount assignAcct : assignment.getAssignmentAccounts()){
+			assignAcct.setTkAssignAcctId(null);
+			assignAcct.setTkAssignmentId(assignment.getTkAssignmentId());
+		}
+	}
+
+}
