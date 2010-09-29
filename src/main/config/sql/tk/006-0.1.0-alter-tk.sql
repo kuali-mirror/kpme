5d37f7ddd16d671a69b8014f6cf174ecccf5e2ac
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-09-29 15:49:23 +0000

    added semi colon
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/datamodelchanges@421 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/sql/tk/006-0.1.0-alter-tk.sql
+++ src/main/config/sql/tk/006-0.1.0-alter-tk.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `tk_roles_s` (
   `id` bigint(19) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2085 DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+) ENGINE=MyISAM AUTO_INCREMENT=2085 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 CREATE TABLE `tk_roles_t` (
   `tk_roles_id` bigint(19) NOT NULL,
@@ -14,4 +14,4 @@ CREATE TABLE `tk_roles_t` (
   `timestamp` timestamp NULL DEFAULT NULL,
   `active` bit(1) DEFAULT NULL,
   PRIMARY KEY (`tk_roles_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
