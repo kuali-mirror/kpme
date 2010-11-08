8a49eae56f49a6c2f30c0de0118d928d0d8c7cc6
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-11-08 19:27:21 +0000

    fixed request processor issue with backdoor testing
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@537 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/web/TKRequestProcessor.java
+++ src/main/java/org/kuali/hr/time/web/TKRequestProcessor.java
@@ -57,9 +57,13 @@ public class TKRequestProcessor extends KualiRequestProcessor {
 		if (request != null) {
 			UserSession userSession = UserLoginFilter.getUserSession(request);
 			TKUser tkUser = new TKUser();
-			Person backdoorPerson = userSession.getBackdoorPerson();
-			Person person = userSession.getActualPerson();
-
+			Person person = null;
+			Person backdoorPerson = null;
+			if(userSession!=null){
+				backdoorPerson = userSession.getBackdoorPerson();
+				person = userSession.getActualPerson();
+			}
+			
 			// Check for test mode; if not test mode check for backDoor validity.
 			if (new Boolean(ConfigContext.getCurrentContextConfig().getProperty("test.mode"))) {
 				request.setAttribute("principalName", TkLoginFilter.TEST_ID);
