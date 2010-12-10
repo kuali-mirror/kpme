1ccd559c7cfa303c2646f5e486ef2aa72bbdd36d
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2010-12-10 14:27:58 +0000

    fixed comp error
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@653 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/shiftdiff/rule/service/ShiftDifferentialRuleServiceImpl.java
+++ src/main/java/org/kuali/hr/time/shiftdiff/rule/service/ShiftDifferentialRuleServiceImpl.java
@@ -59,9 +59,9 @@ public class ShiftDifferentialRuleServiceImpl implements ShiftDifferentialRuleSe
 
 		// Get the last day of the last week of the previous pay period.
 		// This is the only day that can have impact on the current day.
-		List<TimeBlock> prevBlocks = TkServiceLocator.getTimesheetService().getPrevDocumentTimeBlocks(timesheetDocument.getPrincipalId(), timesheetDocument.getDocumentHeader().getDocumentId());
+		List<TimeBlock> prevBlocks = TkServiceLocator.getTimesheetService().getPrevDocumentTimeBlocks(timesheetDocument.getPrincipalId(), Long.parseLong(timesheetDocument.getDocumentHeader().getDocumentId()));
 		if (prevBlocks.size() > 0) {
-			TimesheetDocumentHeader prevTdh = TkServiceLocator.getTimesheetDocumentHeaderService().getPreviousDocumentHeader(timesheetDocument.getPrincipalId(), timesheetDocument.getDocumentHeader().getDocumentId());
+			TimesheetDocumentHeader prevTdh = TkServiceLocator.getTimesheetDocumentHeaderService().getPreviousDocumentHeader(timesheetDocument.getPrincipalId(), Long.parseLong(timesheetDocument.getDocumentHeader().getDocumentId()));
 			if (prevTdh != null) {
 				PayCalendarEntries prevPayCalendarEntry = TkServiceLocator.getPayCalendarSerivce().getCurrentPayCalendarDates(timesheetDocument.getPrincipalId(), TKUtils.getTimelessDate(prevTdh.getPayBeginDate()));
 				TkTimeBlockAggregate prevTimeAggregate = new TkTimeBlockAggregate(prevBlocks, prevPayCalendarEntry);
