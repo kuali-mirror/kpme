74d7f6a8cf04dd824ee5eb20dbde04cdb3d4a6f5
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-01-04 21:56:00 +0000

    Fixes to various JIRA issues.  Mostly minor configuration.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@728 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/main/java/org/kuali/hr/time/department/DepartmentRule.java
@@ -0,0 +1,68 @@
+package org.kuali.hr.time.department;
+
+
+import org.kuali.kfs.coa.businessobject.Chart;
+import org.kuali.kfs.coa.businessobject.Organization;
+import org.kuali.rice.kns.bo.PersistableBusinessObject;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+
+public class DepartmentRule extends MaintenanceDocumentRuleBase {
+
+	boolean validateChart(String value) {
+		boolean valid = false;
+		
+		Chart chart = KNSServiceLocator.getBusinessObjectService().findBySinglePrimaryKey(Chart.class, value);
+		valid = (chart != null);
+		
+		if (!valid) {
+			this.putFieldError("chart", "dept.chart.notfound", value);
+		}
+
+		return valid;
+	}
+
+	boolean validateOrg(String value) {
+		boolean valid = false;
+		
+		Organization org = KNSServiceLocator.getBusinessObjectService().findBySinglePrimaryKey(Organization.class, value);
+		valid = (org != null);
+		
+		if (!valid) {
+			this.putFieldError("org", "dept.org.notfound", value);
+		}
+		
+		return valid;
+	}
+
+	/**
+	 * The calling method doesn't seem to examine the return value.
+	 */
+	@Override
+	protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
+		boolean valid = false;
+
+		PersistableBusinessObject pbo = this.getNewBo();
+		if (pbo instanceof Department) {
+			Department clr = (Department) pbo;
+			if (clr != null) {
+				valid = true;
+				valid &= validateChart(clr.getChart());
+				valid &= validateOrg(clr.getOrg());
+			}
+		}
+
+		return valid;
+	}
+
+	@Override
+	protected boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
+		return super.processCustomApproveDocumentBusinessRules(document);
+	}
+
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+		return super.processCustomRouteDocumentBusinessRules(document);
+	}
+}
