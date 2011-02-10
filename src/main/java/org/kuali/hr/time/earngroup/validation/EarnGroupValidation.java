8b6ca33f1d9f69bd45f62f6829c38d104104fcea
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-02-10 20:36:40 +0000

    KPME-219: added Earn Code validation for Earn Group
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@860 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/earngroup/validation/EarnGroupValidation.java
+++ src/main/java/org/kuali/hr/time/earngroup/validation/EarnGroupValidation.java
@@ -1,12 +1,19 @@
 package org.kuali.hr.time.earngroup.validation;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.kuali.hr.time.earngroup.EarnGroup;
 import org.kuali.hr.time.earngroup.EarnGroupDefinition;
 import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.rice.kns.service.KNSServiceLocator;
 
 public class EarnGroupValidation  extends MaintenanceDocumentRuleBase{
 	
@@ -20,9 +27,40 @@ public class EarnGroupValidation  extends MaintenanceDocumentRuleBase{
 				this.putFieldError("earnGroups["+index+"].earnCode", "earngroup.duplicate.earncode",earnGroupDef.getEarnCode());
 						
 			} 
+			if(earnGroup.getShowSummary()) {
+				validateEarnCode(earnGroupDef.getEarnCode(), index);
+			}
 			earnCodes.add(earnGroupDef.getEarnCode());
 			index++;
 		}
 		return true;
 	}
-}
+	
+    protected void validateEarnCode(String earnCode, int index) {
+    	BusinessObjectService businessObjectService = KNSServiceLocator.getBusinessObjectService();
+    	Map<String,Object> criteria = new HashMap<String,Object>();
+		criteria.put("showSummary", "Y");
+		criteria.put("active", "Y");
+    	Collection aCol = businessObjectService.findMatching(EarnGroup.class, criteria);
+		Iterator<EarnGroup> itr = aCol.iterator();
+		while (itr.hasNext()) {
+			EarnGroup earnGroup = itr.next();
+			criteria = new HashMap<String,Object>();
+			criteria.put("tkEarnGroupId", earnGroup.getTkEarnGroupId());
+			
+			Collection earnGroupDefs = businessObjectService.findMatching(EarnGroupDefinition.class, criteria);
+			Iterator<EarnGroupDefinition> iterator = earnGroupDefs.iterator();
+			while (iterator.hasNext()) {
+				EarnGroupDefinition def = iterator.next();
+				if(StringUtils.equals(earnCode, def.getEarnCode())) {
+					String[] parameters = new String[2];
+					parameters[0] = earnCode;
+					parameters[1] = earnGroup.getDescr();
+					this.putFieldError("earnGroups["+index+"].earnCode", "earngroup.earncode.already.used", parameters);
+				}
+			}
+			
+		}
+    }
+    
+ }
