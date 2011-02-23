c10a27b3a5172d058a4677a937c22fc4d6bdb2fc
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-02-23 19:24:29 +0000

    Fixes issue with EndPayPeriodDateTime not being stored correctly when persisting to database.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@924 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/paycalendar/PayCalendarEntryMaintainableImpl.java
+++ src/main/java/org/kuali/hr/time/paycalendar/PayCalendarEntryMaintainableImpl.java
@@ -9,7 +9,7 @@ import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
 public class PayCalendarEntryMaintainableImpl extends KualiMaintainableImpl {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
@@ -29,26 +29,26 @@ public class PayCalendarEntryMaintainableImpl extends KualiMaintainableImpl {
 		PayCalendarEntries payEntry = (PayCalendarEntries)super.getBusinessObject();
 		java.sql.Date beginDate = payEntry.getBeginPeriodDate();
 		java.sql.Time beginTime = payEntry.getBeginPeriodTime();
-		
+
 		LocalTime beginTimeLocal = new LocalTime(beginTime.getTime(), TkConstants.SYSTEM_DATE_TIME_ZONE);
 		DateTime beginDateTime = new DateTime(beginDate.getTime(), TkConstants.SYSTEM_DATE_TIME_ZONE);
 		beginDateTime = beginDateTime.plus(beginTimeLocal.getMillisOfDay());
 		payEntry.setBeginPeriodDateTime(new java.util.Date(beginDateTime.getMillis()));
-		
+
 		java.sql.Date endDate = payEntry.getEndPeriodDate();
 		java.sql.Time endTime = payEntry.getEndPeriodTime();
-		
+
 		LocalTime endTimeLocal = new LocalTime(endTime.getTime(), TkConstants.SYSTEM_DATE_TIME_ZONE);
 		DateTime endDateTime = new DateTime(endDate.getTime(), TkConstants.SYSTEM_DATE_TIME_ZONE);
 		endDateTime = endDateTime.plus(endTimeLocal.getMillisOfDay());
-		
-		payEntry.setEndPeriodDateTime(new java.util.Date(endDate.getTime()+endTime.getTime()));
+
+		payEntry.setEndPeriodDateTime(new java.util.Date(endDateTime.getMillis()));
 		super.saveBusinessObject();
 	}
 
-	
-	
-	
-	
+
+
+
+
 
 }
