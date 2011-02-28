3f7acfbc1900d65278894d2f23e4e6f150f30162
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-02-28 17:14:33 +0000

    KPME-181 : npadharia : effective date removed from lookup default back in return url
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@945 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/TkLookupableHelper.java
+++ src/main/java/org/kuali/hr/time/TkLookupableHelper.java
@@ -2,10 +2,12 @@ package org.kuali.hr.time;
 
 import java.util.List;
 import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kns.authorization.BusinessObjectRestrictions;
 import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.lookup.HtmlData;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.rice.kns.web.struts.form.LookupForm;
 
 public class TkLookupableHelper extends KualiLookupableHelperServiceImpl {
 
@@ -17,24 +19,35 @@ public class TkLookupableHelper extends KualiLookupableHelperServiceImpl {
 	@Override
 	public List<? extends BusinessObject> getSearchResults(
 			Map<String, String> fieldValues) {
-		if(fieldValues.containsKey("workArea") && StringUtils.equals(fieldValues.get("workArea"),"%")){
+		if (fieldValues.containsKey("workArea")
+				&& StringUtils.equals(fieldValues.get("workArea"), "%")) {
 			fieldValues.put("workArea", "");
 		}
-		if(fieldValues.containsKey("jobNumber") && StringUtils.equals(fieldValues.get("jobNumber"), "%")){
+		if (fieldValues.containsKey("jobNumber")
+				&& StringUtils.equals(fieldValues.get("jobNumber"), "%")) {
 			fieldValues.put("jobNumber", "");
 		}
+
 		return super.getSearchResults(fieldValues);
+	}
+
+	@Override
+	public HtmlData getReturnUrl(BusinessObject businessObject,
+			LookupForm lookupForm, List returnKeys,
+			BusinessObjectRestrictions businessObjectRestrictions) {
+		lookupForm.getFieldConversions().remove("effectiveDate");
+		returnKeys.remove("effectiveDate");
+		return super.getReturnUrl(businessObject, lookupForm, returnKeys,
+				businessObjectRestrictions);
 	}
 
 	@Override
 	protected void validateSearchParameterWildcardAndOperators(
 			String attributeName, String attributeValue) {
-		if(!StringUtils.equals(attributeValue, "%")){
+		if (!StringUtils.equals(attributeValue, "%")) {
 			super.validateSearchParameterWildcardAndOperators(attributeName,
-					attributeValue);	
+					attributeValue);
 		}
 	}
-
-
 
 }
