028dfb2d52e36c2156298321b0bb0e8f144859c2
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-03-09 11:31:54 +0000

    KPME-209 : npadharia : modified table field.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@977 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/sql/tk/024-0.1.0-tk-alter.sql
+++ src/main/config/sql/tk/024-0.1.0-tk-alter.sql
@@ -5,3 +5,4 @@ ALTER TABLE tk_py_calendar_entries_t ADD COLUMN `calendar_group` VARCHAR(45) COL
 ALTER TABLE tk_py_calendar_entries_t change `tk_py_calendar_id` `tk_py_calendar_id` bigint(20) NULL;
 ALTER TABLE tk_holiday_calendar_dates_t MODIFY COLUMN `HOLIDAY_HRS` DECIMAL(4,2) NOT NULL;
 ALTER TABLE tk_holiday_calendar_t ADD COLUMN `location` VARCHAR(30) AFTER `descr` ;
+ALTER TABLE tk_daily_overtime_rl_t MODIFY COLUMN `MAX_GAP` DECIMAL(8,2);
