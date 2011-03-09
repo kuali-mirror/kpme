ca0c9e31d7a103ba24c2b60a1855364b0ac50861
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-03-09 21:45:12 +0000

    Initial pass at KPME-293.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@979 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/timesummary/service/TimeSummaryServiceImpl.java
+++ src/main/java/org/kuali/hr/time/timesummary/service/TimeSummaryServiceImpl.java
@@ -1,8 +1,12 @@
 package org.kuali.hr.time.timesummary.service;
 
-import org.joda.time.LocalTime;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
 import org.kuali.hr.time.assignment.Assignment;
 import org.kuali.hr.time.earngroup.EarnGroup;
+import org.kuali.hr.time.flsa.FlsaDay;
+import org.kuali.hr.time.flsa.FlsaWeek;
+import org.kuali.hr.time.paycalendar.PayCalendar;
 import org.kuali.hr.time.paycalendar.PayCalendarEntries;
 import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.timeblock.TimeBlock;
@@ -18,22 +22,12 @@ import org.kuali.hr.time.util.TkTimeBlockAggregate;
 import java.math.BigDecimal;
 import java.util.*;
 
-public class TimeSummaryServiceImpl implements TimeSummaryService{
+public class TimeSummaryServiceImpl implements TimeSummaryService {
 	private static final String OTHER_EARN_GROUP = "Other";
 
 	@Override
 	public TimeSummary getTimeSummary(TimesheetDocument timesheetDocument) {
-		TimeSummary timeSummary = new TimeSummary();
-
-		if(timesheetDocument.getTimeBlocks() == null) {
-			return timeSummary;
-		}
-		timeSummary.setSummaryHeader(getHeaderForSummary(timesheetDocument.getPayCalendarEntry()));
-		TkTimeBlockAggregate tkTimeBlockAggregate = new TkTimeBlockAggregate(timesheetDocument.getTimeBlocks(), timesheetDocument.getPayCalendarEntry());
-		timeSummary.setWorkedHours(getWorkedHours(tkTimeBlockAggregate));
-		timeSummary.setSections(buildSummarySections(tkTimeBlockAggregate,timesheetDocument.getPayCalendarEntry().getBeginPeriodDateTime(),timesheetDocument));
-
-		return timeSummary;
+		return this.getTimeSummary(timesheetDocument, timesheetDocument.getTimeBlocks());
 	}
 
 	@Override
@@ -43,85 +37,178 @@ public class TimeSummaryServiceImpl implements TimeSummaryService{
 		if(timesheetDocument.getTimeBlocks() == null) {
 			return timeSummary;
 		}
-		timeSummary.setSummaryHeader(getHeaderForSummary(timesheetDocument.getPayCalendarEntry()));
+
+        // This variable data is generated in getHeaderFOrSummary()
+        // We are already touching the calendar and figuring out the boundaries
+        // in that method, so to reduce double work we'll get this info there as well.
+        //
+        // This could be refactored to generate the boolean list first, and then
+        // use that list to generate the other values with another method call.
+        //
+        // For future implementation!
+        List<Boolean> dayArrangements = new ArrayList<Boolean>();
+
+		timeSummary.setSummaryHeader(getHeaderForSummary(timesheetDocument.getPayCalendarEntry(), dayArrangements));
 		TkTimeBlockAggregate tkTimeBlockAggregate = new TkTimeBlockAggregate(timeBlocks, timesheetDocument.getPayCalendarEntry());
 		timeSummary.setWorkedHours(getWorkedHours(tkTimeBlockAggregate));
-		timeSummary.setSections(buildSummarySections(tkTimeBlockAggregate,timesheetDocument.getPayCalendarEntry().getBeginPeriodDateTime(),timesheetDocument));
+        List<EarnGroupSection> sections = buildSummarySections(dayArrangements, tkTimeBlockAggregate, timesheetDocument.getPayCalendarEntry().getBeginPeriodDateTime(), timesheetDocument);
+        timeSummary.setSections(sections);
 
 		return timeSummary;
 	}
 
-	private List<BigDecimal> getWorkedHours(TkTimeBlockAggregate timeBlockAggregate){
-		List<BigDecimal> workedHours = new ArrayList<BigDecimal>();
-		int dayCount = 1;
-		BigDecimal weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
-		BigDecimal periodTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
-		for(List<TimeBlock> lstDayTimeblocks : timeBlockAggregate.getDayTimeBlockList()){
-			BigDecimal totalForDay = TkConstants.BIG_DECIMAL_SCALED_ZERO;
-			for(TimeBlock tb : lstDayTimeblocks){
-				totalForDay = totalForDay.add(tb.getHours(), TkConstants.MATH_CONTEXT);
-				weeklyTotal = weeklyTotal.add(tb.getHours(), TkConstants.MATH_CONTEXT);
-				periodTotal = periodTotal.add(tb.getHours(), TkConstants.MATH_CONTEXT);
-			}
-			workedHours.add(totalForDay);
-			if((dayCount % 7) == 0){
-				workedHours.add(weeklyTotal);
-				weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
-			}
-			dayCount++;
-		}
-		workedHours.add(periodTotal);
-		return workedHours;
-	}
 
-	private List<String> getHeaderForSummary(PayCalendarEntries payEntry){
-		// Iterate over date range and setup a list for the header
-		Calendar payBeginCal = GregorianCalendar.getInstance();
-		Calendar payEndCal = GregorianCalendar.getInstance();
-		payBeginCal.setTime(payEntry.getBeginPeriodDateTime());
-		payEndCal.setTime(payEntry.getEndPeriodDateTime());
+    /**
+     * Provides the number of hours worked for the pay period indicated in the
+     * aggregate. Break down is by FLSA day.
+     *
+     * @param aggregate The aggregate we are summing
+     *
+     * @return A list of BigDecimals containing the number of hours worked.
+     * This list will line up with the header.
+     */
+    private List<BigDecimal> getWorkedHours(TkTimeBlockAggregate aggregate) {
+        List<BigDecimal> hours = new ArrayList<BigDecimal>();
+        BigDecimal periodTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
 
-		List<String> summaryHeader = new ArrayList<String>();
+        for (FlsaWeek week : aggregate.getFlsaWeeks()) {
+            BigDecimal weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
+            for (FlsaDay day : week.getFlsaDays()) {
+                BigDecimal totalForDay = TkConstants.BIG_DECIMAL_SCALED_ZERO;
+                for (TimeBlock block : day.getAppliedTimeBlocks()) {
+                    totalForDay = totalForDay.add(block.getHours(), TkConstants.MATH_CONTEXT);
+                    weeklyTotal = weeklyTotal.add(block.getHours(), TkConstants.MATH_CONTEXT);
+                    periodTotal = periodTotal.add(block.getHours(), TkConstants.MATH_CONTEXT);
+                }
+                hours.add(totalForDay);
+            }
+            hours.add(weeklyTotal);
+        }
+        hours.add(periodTotal);
 
-        // This bit of code accounts for virtual days in calendars that may
-        // not be from 00:00:00 to 00:00:00.
-        LocalTime boundaryTime = LocalTime.fromDateFields(payEntry.getEndPeriodDateTime());
-        boolean showLastDay = true;
-        if (boundaryTime.getHourOfDay() == 0 && boundaryTime.getMinuteOfHour() == 0 && boundaryTime.getSecondOfMinute() == 0) {
-            showLastDay = false;
+        return hours;
+    }
+
+
+    /**
+     * Handles the generation of the display header for the time summary.
+     *
+     * @param cal The PayCalendarEntries object we are using to derive information.
+     * @param dayArrangements Container passed in to store the position of week / period aggregate sums
+     *
+     * @return An in-order string of days for this period that properly accounts
+     * for FLSA week boundaries in the pay period.
+     */
+    private List<String> getHeaderForSummary(PayCalendarEntries cal, List<Boolean> dayArrangements) {
+        List<String> header = new ArrayList<String>();
+
+        // Maps directly to joda time day of week constants.
+        int flsaBeginDay = this.getPayCalendarForEntry(cal).getFlsaBeginDayConstant();
+        boolean virtualDays = false;
+        DateTime startDate = new DateTime(cal.getBeginPeriodDateTime());
+        DateTime endDate = new DateTime(cal.getEndPeriodDateTime());
+
+        // Increment end date if we are on a virtual day calendar, so that the
+        // for loop can account for having the proper amount of days on the
+        // summary calendar.
+        if (endDate.get(DateTimeFieldType.hourOfDay()) != 0 || endDate.get(DateTimeFieldType.minuteOfHour()) != 0 ||
+                endDate.get(DateTimeFieldType.secondOfMinute()) != 0)
+        {
+            endDate = endDate.plusDays(1);
+            virtualDays = true;
         }
 
-		int dayCount = 1;
-		//Build up map for date to day description
+        boolean afterFirstDay = false;
+        int week = 1;
+        for (DateTime currentDate = startDate; currentDate.compareTo(endDate) < 0; currentDate = currentDate.plusDays(1)) {
 
-		while (payBeginCal.before(payEndCal) || (showLastDay && payBeginCal.equals(payEndCal)) ) {
-			String displayName = (payBeginCal.get(Calendar.MONTH) + 1) + "/"
-							+ payBeginCal.get(Calendar.DAY_OF_MONTH);
-			payBeginCal.add(Calendar.DAY_OF_MONTH, 1);
-			if(TKUtils.isVirtualWorkDay(payBeginCal)){
-				displayName += " - " + (payBeginCal.get(Calendar.MONTH) + 1) + "/"
-								+ payBeginCal.get(Calendar.DAY_OF_MONTH);
-			}
-			summaryHeader.add(displayName);
-			//Add weekly total column
-			if ((dayCount % 7) == 0) {
-				summaryHeader.add("Week " + (dayCount / 7));
-			}
-			dayCount++;
+            if (currentDate.getDayOfWeek() == flsaBeginDay && afterFirstDay) {
+                header.add("Week " + week);
+                dayArrangements.add(false);
+                week++;
+            }
 
-		}
-		// add period total column
-		summaryHeader.add("Period Total");
-		return summaryHeader;
-	}
+            header.add(makeHeaderDiplayString(currentDate, virtualDays));
+            dayArrangements.add(true);
 
-	private List<EarnGroupSection> buildSummarySections(TkTimeBlockAggregate timeBlockAggregate, Date asOfDate, TimesheetDocument timesheetDocument){
+
+            afterFirstDay = true;
+        }
+
+        // We may have a very small final "week" on this pay period. For now
+        // we will mark it as a week, and if someone doesn't like it, it can
+        // be removed.
+        if (!header.get(header.size()-1).startsWith("Week")) {
+            dayArrangements.add(false);
+            header.add("Week " + week);
+        }
+
+
+        header.add("Period Total");
+        dayArrangements.add(false);
+        return header;
+    }
+
+    /**
+     * Helper function to generate display text for the summary header.
+     * @param currentDate The date we are generating for.
+     * @param virtualDays Whether or not virtual days apply.
+     * @return A string appropriate for UI display.
+     */
+    private String makeHeaderDiplayString(DateTime currentDate, boolean virtualDays) {
+        StringBuffer display = new StringBuffer();
+        display.append(currentDate.get(DateTimeFieldType.monthOfYear())).append('/');
+        display.append(currentDate.get(DateTimeFieldType.dayOfMonth()));
+
+        if (virtualDays) {
+            display.append(" - ");
+            display.append(currentDate.get(DateTimeFieldType.monthOfYear())).append('/');
+            display.append(currentDate.get(DateTimeFieldType.dayOfMonth()) + 1);
+        }
+
+        return display.toString();
+    }
+
+    /**
+     * @param calEntry Calendar entry we are using for lookup.
+     * @return The PayCalendar that owns the provided entry.
+     */
+    private PayCalendar getPayCalendarForEntry(PayCalendarEntries calEntry) {
+        PayCalendar cal = null;
+
+        if (calEntry != null) {
+            cal = TkServiceLocator.getPayCalendarSerivce().getPayCalendar(calEntry.getPayCalendarId());
+        }
+
+        return cal;
+    }
+
+
+    /**
+     * This method is responsible for grouping the earn codes and assignments
+     * into rows that can be displayed on the user interface.
+     *
+     * @param dayArrangements List of true/false values that let us know whether
+     * or not the day at the index position should be a weekly / period summary
+     * or just a day sum. This is to allow for FLSA weeks being uneven.
+     *
+     * @param timeBlockAggregate The time/blocks we are summarizing.
+     * @param asOfDate
+     * @param timesheetDocument
+     *
+     * @return A list of EarnGroupSelection objects containing the processed
+     * information from the timeBlockAggregate.
+     */
+	private List<EarnGroupSection> buildSummarySections(List<Boolean> dayArrangements, TkTimeBlockAggregate timeBlockAggregate, Date asOfDate, TimesheetDocument timesheetDocument){
 		Map<Integer,Map<String,BigDecimal>> dayToEarnGroupAssignToHoursMap = new HashMap<Integer,Map<String,BigDecimal>>();
 		Map<String,Set<String>> earnGroupToAssignmentSets = new HashMap<String,Set<String>>();
 		List<EarnGroupSection> lstEarnGroupSections = new ArrayList<EarnGroupSection>();
 
+        // This could stand to be refactored - we don't really care about day
+        // counts anymore. We now are interested in FLSA day boundaries and these
+        // have already been computed.
+        //
 		int dayCount = 1;
-
 		for(List<TimeBlock> timeBlocksForDay : timeBlockAggregate.getDayTimeBlockList()){
 			Map<String,BigDecimal> earnGroupAssignToHoursMap = null;
 			for(TimeBlock tb : timeBlocksForDay){
@@ -146,6 +233,9 @@ public class TimeSummaryServiceImpl implements TimeSummaryService{
 		}
 
 
+        // Here we are building up the columns in the summary section that
+        // contain hours and sums of hours for weeks and periods.
+        //
 		for(String earnGroup : earnGroupToAssignmentSets.keySet()){
 			//for each assignment
 			EarnGroupSection earnGroupSection = new EarnGroupSection();
@@ -156,21 +246,46 @@ public class TimeSummaryServiceImpl implements TimeSummaryService{
 				assignRow.setDescr(assign.getAssignmentDescription());
 				BigDecimal weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
 				BigDecimal periodTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
-				for(int i = 1; i< dayToEarnGroupAssignToHoursMap.size()+1;i++){
+
+                // this counter is used to know what position in the dayArrangements
+                // boolean list we should use.
+                //
+                // Note that daydaycount is out of sync with 'i' -> this is something
+                // that we can refactor now that we are not under the assumption of
+                // always having 7 day weeks.
+                int daydaycount = 0;
+				for(int i = 1; i <= dayToEarnGroupAssignToHoursMap.size(); i++){ // replace this with std. iterator after refactor above storage code
 					Map<String,BigDecimal> earnGroupAssignToHoursMap = dayToEarnGroupAssignToHoursMap.get(i);
+                    Boolean dayIsDay = dayArrangements.get(daydaycount); // is this day 'i', a Day? (or a week/period summary)
 					BigDecimal hrs = TkConstants.BIG_DECIMAL_SCALED_ZERO;
 					if(earnGroupAssignToHoursMap != null && earnGroupAssignToHoursMap.get(earnGroup+"_"+assignmentDescr)!=null){
 						hrs = earnGroupAssignToHoursMap.get(earnGroup+"_"+assignmentDescr);
 					}
 
-					assignRow.getTotal().add(hrs);
-					weeklyTotal = weeklyTotal.add(hrs,TkConstants.MATH_CONTEXT);
-					periodTotal = periodTotal.add(hrs, TkConstants.MATH_CONTEXT);
-					if((i % 7)== 0){
+                    // Use our pre-generated list to determine whether this position
+                    // is a week/period summary or a day.
+                    if (!dayIsDay) {
 						assignRow.getTotal().add(weeklyTotal);
 						weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
+                        daydaycount++; // bump to catch up. The map only contains time block info not period summaries.
 					}
+
+                    assignRow.getTotal().add(hrs);
+                    weeklyTotal = weeklyTotal.add(hrs,TkConstants.MATH_CONTEXT);
+                    periodTotal = periodTotal.add(hrs, TkConstants.MATH_CONTEXT);
+
+                    daydaycount++; // bump at loop iteration
+
+                    if (i == dayToEarnGroupAssignToHoursMap.size()) {
+                        // check for final week sum
+                        dayIsDay = dayArrangements.get(daydaycount);
+                        if (!dayIsDay) {
+                            assignRow.getTotal().add(weeklyTotal);
+                            weeklyTotal = TkConstants.BIG_DECIMAL_SCALED_ZERO;
+                        }
+                    }
 				}
+
 				assignRow.getTotal().add(periodTotal);
 				earnGroupSection.addAssignmentRow(assignRow);
 			}
@@ -206,7 +321,5 @@ public class TimeSummaryServiceImpl implements TimeSummaryService{
 
 		return earnGroupAssignToHoursMap;
 	}
-
-
 
 }
