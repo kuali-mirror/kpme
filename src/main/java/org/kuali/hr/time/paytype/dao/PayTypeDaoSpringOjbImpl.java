e6c616ffb1992aeb1a90de26de48e3658d789137
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-03-31 08:10:18 +0000

    KPME-359 : npadharia : Timestamp and effective date added to fetching paytype method.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1030 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/paytype/dao/PayTypeDaoSpringOjbImpl.java
+++ src/main/java/org/kuali/hr/time/paytype/dao/PayTypeDaoSpringOjbImpl.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
 import org.apache.ojb.broker.query.QueryFactory;
+import org.apache.ojb.broker.query.ReportQueryByCriteria;
 import org.kuali.hr.time.paytype.PayType;
 import org.springmodules.orm.ojb.support.PersistenceBrokerDaoSupport;
 
@@ -27,11 +29,28 @@ public class PayTypeDaoSpringOjbImpl extends PersistenceBrokerDaoSupport impleme
 
 	public PayType getPayType(String payType, Date effectiveDate) {
 		Criteria currentRecordCriteria = new Criteria();
+		Criteria effdt = new Criteria();
+		Criteria timestamp = new Criteria();
 		
-		currentRecordCriteria.addEqualTo("payType", payType);
-		currentRecordCriteria.addLessOrEqualThan("effectiveDate", effectiveDate);
-		//TODO timestamp max here
+		effdt.addEqualToField("payType", Criteria.PARENT_QUERY_PREFIX + "payType");
+		effdt.addLessOrEqualThan("effectiveDate", effectiveDate);
+		ReportQueryByCriteria effdtSubQuery = QueryFactory.newReportQuery(PayType.class, effdt);
+		effdtSubQuery.setAttributes(new String[] { "max(effdt)" });
 
-		return (PayType) this.getPersistenceBrokerTemplate().getObjectByQuery(QueryFactory.newQuery(PayType.class, currentRecordCriteria));
+		timestamp.addEqualToField("payType", Criteria.PARENT_QUERY_PREFIX + "payType");
+		timestamp.addEqualToField("effectiveDate", Criteria.PARENT_QUERY_PREFIX + "effectiveDate");
+		ReportQueryByCriteria timestampSubQuery = QueryFactory.newReportQuery(PayType.class, timestamp);
+		timestampSubQuery.setAttributes(new String[] { "max(timestamp)" });
+
+		currentRecordCriteria.addEqualTo("payType", payType);
+		currentRecordCriteria.addEqualTo("effectiveDate", effdtSubQuery);
+		currentRecordCriteria.addEqualTo("timestamp", timestampSubQuery);
+		
+		Criteria activeFilter = new Criteria(); // Inner Join For Activity
+		activeFilter.addEqualTo("active", true);
+		currentRecordCriteria.addAndCriteria(activeFilter);
+
+		Query query = QueryFactory.newQuery(PayType.class, currentRecordCriteria);
+		return (PayType)this.getPersistenceBrokerTemplate().getObjectByQuery(query);
 	}
 }
