6d4035e0d53b1c0ebb9ecef096eead2bec775da5
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-04-05 14:40:43 +0000

    selenium JUnit test
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1039 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/time/web/UICalendarTest.java
+++ src/test/java/org/kuali/hr/time/web/UICalendarTest.java
@@ -1,50 +1,41 @@
 package org.kuali.hr.time.web;
 
-import org.junit.Ignore;
-import org.openqa.selenium.server.SeleniumServer;
-
 import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.SeleneseTestCase;
-import com.thoughtworks.selenium.Selenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-@Ignore
 public class UICalendarTest extends SeleneseTestCase {
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080/");
+        try {
+            selenium.start();
+        } catch (Exception e) {
 
-	private static final String BASE_URL = "http://localhost:8080/tk-dev/";
-	private Selenium selenium = new DefaultSelenium("localhost", 4444, "*firefox3 C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe", BASE_URL);
-	private SeleniumServer seleniumServer;
+        }
+    }
 
-	public void setUp() throws Exception {
-		seleniumServer = new SeleniumServer();
-	    seleniumServer.start();
-	    selenium.start();
-	}
+    @Test
+    public void testAddTimeBlock() throws Exception {
+        selenium.open("/tk-dev/TimeDetail.do");
+        selenium.type("__login_user", "fran");
+        selenium.click("//input[@name='login']");
+        selenium.waitForPageToLoad("30000");
+        selenium.mouseDownAt("css=td.fc-day4", "10,10");
+        selenium.mouseUpAt("css=td.fc-day4", "10,10");
+        selenium.type("beginTimeField", "08:15 AM");
+        selenium.type("endTimeField", "05:15 PM");
+        selenium.click("//button[@type='button']");
+        selenium.waitForPageToLoad("30000");
+        verifyTrue(selenium.isTextPresent("08:15 AM"));
+        verifyTrue(selenium.isTextPresent("05:15 PM"));
+    }
 
-	public void tearDown() throws Exception {
-		 selenium.stop();
-	     seleniumServer.stop();
-	}
-
-	public void testCalendar() throws Exception {
-
-		selenium.open(BASE_URL + "TimeDetail.do");
-
-		selenium.click("//input[@name='login']");
-		selenium.waitForPageToLoad("5000");
-		selenium.mouseDownAt("css=td.fc-day1", "0,0");
-		selenium.mouseUpAt("css=td.fc-day1", "0,0");
-		selenium.type("beginTimeField", "08:00 AM");
-		selenium.type("endTimeField", "05:00 PM");
-		selenium.click("acrossDays");
-		selenium.click("//div[11]/button[1]");
-		for (int second = 0;; second++) {
-			if (second >= 60) fail("timeout");
-			try { if (selenium.isTextPresent("HRMS Java Team : RGNX")) break; } catch (Exception e) {}
-			Thread.sleep(1000);
-		}
-
-		selenium.mouseOver("//a[contains(@id,'delete-link')]");
-		selenium.clickAt("//a[contains(@id,'delete-link')]", "0,0");
-	}
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
 }
 
