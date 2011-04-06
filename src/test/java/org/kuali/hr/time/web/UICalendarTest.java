9f6eb1680df0b44bf02aabff0a0f7534cdc5bac5
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-04-06 20:58:42 +0000

    Ignore the entire selenium test class
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1045 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/time/web/UICalendarTest.java
+++ src/test/java/org/kuali/hr/time/web/UICalendarTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 
+@Ignore
 public class UICalendarTest extends SeleneseTestCase {
     @Before
     public void setUp() throws Exception {
