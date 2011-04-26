48ebeaa57c59e6052a6f9e4e6cfbcbce6d053ee0
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-04-26 19:15:12 +0000

    Batch Changes - added query to grab window of PayCalendarEntries.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1109 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/batch/BatchJobManagerThread.java
+++ src/main/java/org/kuali/hr/time/batch/BatchJobManagerThread.java
@@ -10,20 +10,20 @@ import org.kuali.hr.time.util.TKUtils;
 public class BatchJobManagerThread extends Thread {
 	//This represents a number of days on both sides of today
 	public static int numOfDaysToPoll = 30;
-	
+
 	@Override
 	public void run() {
 		Date asOfDate = TKUtils.getCurrentDate();
-		List<PayCalendarEntries> lstPayCalendarEntries = TkServiceLocator.getPayCalendarEntriesSerivce().getCurrentPayCalendarEntryNeedsScheduled(asOfDate);
+		List<PayCalendarEntries> lstPayCalendarEntries = TkServiceLocator.getPayCalendarEntriesSerivce().getCurrentPayCalendarEntryNeedsScheduled(numOfDaysToPoll, asOfDate);
 		for(PayCalendarEntries payCalendarEntry: lstPayCalendarEntries){
 			//Compare against each batch job column and make the associated job entry if needed
 			//Write a query that checks table for the batch job and the pay calendar entry id
 		}
-		
+
 		//Iterate over batch jobs and run job
-	
+
 	}
-	
+
 
 
 }
