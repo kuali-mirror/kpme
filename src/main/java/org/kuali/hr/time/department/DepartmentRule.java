81694fa39c880d2a2941b85a6d54d45717204b14
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-04-27 13:19:23 +0000

    KPME-413 : npadharia : chart and org made non-required
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1117 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/department/DepartmentRule.java
+++ src/main/java/org/kuali/hr/time/department/DepartmentRule.java
@@ -1,6 +1,5 @@
 package org.kuali.hr.time.department;
 
-
 import org.apache.commons.lang.StringUtils;
 import org.kuali.hr.time.roles.TkRole;
 import org.kuali.hr.time.util.TkConstants;
@@ -17,81 +16,90 @@ import java.util.Map;
 
 public class DepartmentRule extends MaintenanceDocumentRuleBase {
 
-    boolean validateChart(String value) {
-        boolean valid = false;
+	boolean validateChart(String value) {
+		boolean valid = true;
+		if (value != null) {
+			Chart chart = KNSServiceLocator.getBusinessObjectService()
+					.findBySinglePrimaryKey(Chart.class, value);
+			valid = (chart != null);
 
-        Chart chart = KNSServiceLocator.getBusinessObjectService().findBySinglePrimaryKey(Chart.class, value);
-        valid = (chart != null);
+			if (!valid) {
+				this.putFieldError("chart", "dept.chart.notfound", value);
+			}
+		}
+		return valid;
+	}
 
-        if (!valid) {
-            this.putFieldError("chart", "dept.chart.notfound", value);
-        }
+	boolean validateOrg(String value) {
+		boolean valid = true;
+		if (value != null) {
+			Map<String, String> primaryKeys = new HashMap<String, String>();
+			primaryKeys.put("organizationCode", value);
+			Organization org = (Organization) KNSServiceLocator
+					.getBusinessObjectService().findByPrimaryKey(
+							Organization.class, primaryKeys); // findBySinglePrimaryKey(Organization.class,
+			// value);
+			valid = (org != null);
 
-        return valid;
-    }
+			if (!valid) {
+				this.putFieldError("org", "dept.org.notfound", value);
+			}
+		}
+		return valid;
+	}
 
-    boolean validateOrg(String value) {
-        boolean valid = false;
+	/**
+	 * Checks for not null and size > 0, but follows the validation pattern
+	 * given in this class for future expanding.
+	 * 
+	 * @return true if there is a role, false otherwise.
+	 */
+	boolean validateRolePresent(List<TkRole> roles) {
+		boolean valid = false;
 
-        Map<String, String> primaryKeys = new HashMap<String, String>();
-        primaryKeys.put("organizationCode", value);
-        Organization org = (Organization) KNSServiceLocator.getBusinessObjectService().findByPrimaryKey(Organization.class, primaryKeys); //findBySinglePrimaryKey(Organization.class, value);
-        valid = (org != null);
+		if (roles != null && roles.size() > 0) {
+			for (TkRole role : roles) {
+				valid |= role.isActive()
+						&& StringUtils.equals(role.getRoleName(),
+								TkConstants.ROLE_TK_ORG_ADMIN);
+			}
+		}
 
-        if (!valid) {
-            this.putFieldError("org", "dept.org.notfound", value);
-        }
+		if (!valid) {
+			this.putGlobalError("role.required");
+		}
 
-        return valid;
-    }
+		return valid;
+	}
 
-    /**
-     * Checks for not null and size > 0, but follows the validation pattern given in this class for future
-     * expanding.
-     *
-     * @return true if there is a role, false otherwise.
-     */
-    boolean validateRolePresent(List<TkRole> roles) {
-        boolean valid = false;
+	/**
+	 * The calling method doesn't seem to examine the return value.
+	 */
+	@Override
+	protected boolean processCustomSaveDocumentBusinessRules(
+			MaintenanceDocument document) {
+		boolean valid = false;
 
-        if (roles != null && roles.size() > 0) {
-            for (TkRole role : roles) {
-                valid |= role.isActive() && StringUtils.equals(role.getRoleName(), TkConstants.ROLE_TK_ORG_ADMIN);
-            }
-        }
+		PersistableBusinessObject pbo = this.getNewBo();
+		if (pbo instanceof Department) {
+			Department clr = (Department) pbo;
+			valid = validateChart(clr.getChart());
+			valid &= validateOrg(clr.getOrg());
+			valid &= validateRolePresent(clr.getRoles());
+		}
 
-        if (!valid) {
-            this.putGlobalError("role.required");
-        }
+		return valid;
+	}
 
-        return valid;
-    }
+	@Override
+	protected boolean processCustomApproveDocumentBusinessRules(
+			MaintenanceDocument document) {
+		return super.processCustomApproveDocumentBusinessRules(document);
+	}
 
-    /**
-     * The calling method doesn't seem to examine the return value.
-     */
-    @Override
-    protected boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
-        boolean valid = false;
-
-        PersistableBusinessObject pbo = this.getNewBo();
-        if (pbo instanceof Department) {
-            Department clr = (Department) pbo;
-            valid = validateChart(clr.getChart());
-            valid &= validateOrg(clr.getOrg());
-            valid &= validateRolePresent(clr.getRoles());
-        }
-
-        return valid;
-    }
-
-    @Override
-    protected boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
-        return super.processCustomApproveDocumentBusinessRules(document);
-    }
-
-    @Override
-    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
-        return super.processCustomRouteDocumentBusinessRules(document);
-    }
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(
+			MaintenanceDocument document) {
+		return super.processCustomRouteDocumentBusinessRules(document);
+	}
 }
