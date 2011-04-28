32548eeaf49e3f5781ba67e8608c4012b1a84e0a
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-04-28 14:24:08 +0000

    finalized initiate job
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1128 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/batch/InitiateBatchJobRunnable.java
+++ src/main/java/org/kuali/hr/time/batch/InitiateBatchJobRunnable.java
@@ -4,14 +4,13 @@ import org.kuali.hr.time.paycalendar.PayCalendarEntries;
 import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.rice.kew.exception.WorkflowException;
 import org.mortbay.log.Log;
+import org.openqa.selenium.internal.seleniumemulation.GetBodyText;
 
 public class InitiateBatchJobRunnable extends BatchJobEntryRunnable {
 
 	@Override
 	public void run() {
-		//TODO Fetch the BatchJobEntry 
-		BatchJobEntry initiateBatchJobEntry = null;
-		
+		BatchJobEntry initiateBatchJobEntry = TkServiceLocator.getBatchJobEntryService().getBatchJobEntry(getTkBatchJobEntryId());
 		String principalId = initiateBatchJobEntry.getPrincipalId();
 		Long payCalendarId = initiateBatchJobEntry.getPayCalendarId();
 		PayCalendarEntries payCalendarEntry = TkServiceLocator.getPayCalendarEntriesSerivce().getPayCalendarEntries(payCalendarId);
