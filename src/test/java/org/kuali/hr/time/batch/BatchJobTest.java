822bbaa8fa9e94d80ac4174efca28f974b1c0e0b
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-05-03 19:26:40 +0000

    Missed files
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1143 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/time/batch/BatchJobTest.java
+++ src/test/java/org/kuali/hr/time/batch/BatchJobTest.java
@@ -8,28 +8,28 @@ import org.kuali.hr.time.test.TkTestCase;
 import org.kuali.hr.time.util.TkConstants;
 
 public class BatchJobTest extends TkTestCase {
-	
+
 	private static final String BATCH_JOB_NAME = "testJob";
 	private Long ibjId;
 	private BatchJobService bjService;
-	
+
 	@Before
     public void setUp() throws Exception {
     	super.setUp();
     	bjService = TkServiceLocator.getBatchJobService();
     }
 	private void creatAndSaveBatchJob(){
-		InitiateBatchJob ibj = new InitiateBatchJob();
+		InitiateBatchJob ibj = new InitiateBatchJob(5L);
 		ibj.setBatchJobName(BATCH_JOB_NAME);
 		ibj.setBatchJobStatus(TkConstants.BATCH_JOB_ENTRY_STATUS.SCHEDULED);
 		ibj.setTimeElapsed(new Long(123));
 		ibj.setPayCalendarEntryId(new Long(1234));
-	
+
 		bjService.saveBatchJob(ibj);
-		
+
 		ibjId = ibj.getTkBatchJobId();
 	}
-	
+
 	@Test
 	public void testSavingAndRetrievingBatchJob() throws Exception {
 		creatAndSaveBatchJob();
