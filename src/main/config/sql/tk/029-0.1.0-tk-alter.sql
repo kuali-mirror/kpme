756ec560a84ca6154499de012a35ad46ad88d093
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-05-04 15:32:04 +0000

    KPME-434 : npadharia : db alteration file created new instead of appending to old file.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1150 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/sql/tk/029-0.1.0-tk-alter.sql
+++ src/main/config/sql/tk/029-0.1.0-tk-alter.sql
@@ -2,5 +2,3 @@ ALTER TABLE `tk_missed_punch_t` ADD COLUMN `timesheet_doc_id` VARCHAR(14) NOT NU
 
 ALTER TABLE `tk_missed_punch_t` ENGINE = InnoDB , CHANGE COLUMN `action_time` `action_time` TIME NOT NULL DEFAULT '00:00:00'  ;
 
-ALTER TABLE `tk_grace_period_rl_t` MODIFY COLUMN `HOUR_FACTOR` DECIMAL(6,0) DEFAULT NULL;
-
