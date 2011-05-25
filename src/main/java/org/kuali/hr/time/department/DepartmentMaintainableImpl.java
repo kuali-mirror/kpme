1b5a31ee0beee2954256d14fc3e573814a4b85a4
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-05-25 22:03:29 +0000

    kpme-540: added validation for principalId of roles on Department
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1220 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/department/DepartmentMaintainableImpl.java
+++ src/main/java/org/kuali/hr/time/department/DepartmentMaintainableImpl.java
@@ -3,10 +3,14 @@ package org.kuali.hr.time.department;
 import org.kuali.hr.time.roles.TkRole;
 import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.util.TKContext;
+import org.kuali.rice.kim.bo.Person;
+import org.kuali.rice.kim.service.KIMServiceLocator;
 import org.kuali.rice.kns.bo.PersistableBusinessObject;
 import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
 import org.kuali.rice.kns.service.KNSServiceLocator;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KNSConstants;
 
 import java.util.List;
 import java.util.Map;
@@ -51,4 +55,22 @@ public class DepartmentMaintainableImpl extends KualiMaintainableImpl {
         TkServiceLocator.getDepartmentService().populateDepartmentRoles(dOld);
         TkServiceLocator.getDepartmentService().populateDepartmentRoles(dNew);
     }
+	
+	@Override
+    public void addNewLineToCollection(String collectionName) {
+        if (collectionName.equals("roles")) {
+        	TkRole aRole = (TkRole)newCollectionLines.get(collectionName );
+            if ( aRole != null ) {
+            	if(!aRole.getPrincipalId().isEmpty()) {
+            		Person aPerson = KIMServiceLocator.getPersonService().getPerson(aRole.getPrincipalId());
+            		if(aPerson == null) {
+            			GlobalVariables.getMessageMap().putErrorWithoutFullErrorPath(KNSConstants.MAINTENANCE_NEW_MAINTAINABLE +"roles", 
+                				"dept.role.person.notExist",aRole.getPrincipalId());
+                		return;
+            		}
+            	}
+            }
+        }
+        super.addNewLineToCollection(collectionName);
+	}
 }
