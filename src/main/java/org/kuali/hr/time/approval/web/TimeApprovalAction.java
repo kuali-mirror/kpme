c7c2d431db719a0eb09facda93d16b99943c5849
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-06-08 19:02:52 +0000

    Cross Developer Sync -- checkpointing to move forward.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1275 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/approval/web/TimeApprovalAction.java
+++ src/main/java/org/kuali/hr/time/approval/web/TimeApprovalAction.java
@@ -1,6 +1,5 @@
 package org.kuali.hr.time.approval.web;
 
-import org.apache.ojb.broker.query.Criteria;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -9,81 +8,67 @@ import org.kuali.hr.time.base.web.TkAction;
 import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.util.TKContext;
 import org.kuali.hr.time.util.TKUser;
-import org.kuali.hr.time.util.TKUtils;
-import org.kuali.hr.time.workflow.TimesheetDocumentHeader;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TimeApprovalAction extends TkAction {
 
     @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form,
-                                 HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         TimeApprovalActionForm taaf = (TimeApprovalActionForm) form;
 
-        taaf.setDocHeaders(TkServiceLocator.getTimesheetDocumentHeaderService().getDocumentHeadersByField(taaf.getSearchField(), taaf.getTerm()));
+        //taaf.setPayCalendarLabels(TkServiceLocator.getTimeApproveService().getPayCalendarLabelsForApprovalTab(taaf.getPayBeginDate(), taaf.getPayEndDate()));
+        TKUser user = TKContext.getUser();
+        taaf.setName(user.getPrincipalName());
+        taaf.setApprovalRows(getApprovalRows(taaf.getTerm(), taaf.isAscending(), taaf.getRows(), taaf.getPayBeginDate(), taaf.getPayEndDate()));
 
         return super.execute(mapping, form, request, response);
     }
 
-    public ActionForward load(ActionMapping mapping, ActionForm form,
-                              HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+    public ActionForward getMoreDocument(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         TimeApprovalActionForm taaf = (TimeApprovalActionForm) form;
-        TKUser tkUser = TKContext.getUser();
-        taaf.setName(tkUser.getCurrentPerson().getName());
-        taaf.setPayBeginDate(TKUtils.createDate(10, 1, 2011, 0, 0, 0));
-        taaf.setPayEndDate(TKUtils.createDate(10, 15, 2011, 0, 0, 0));
 
-        taaf.setPayCalendarLabels(TkServiceLocator.getTimeApproveService().getPayCalendarLabelsForApprovalTab(taaf.getPayBeginDate(), taaf.getPayEndDate()));
-        taaf.setApprovalTimeSummaryRows(TkServiceLocator.getTimeApproveService().getApprovalSummaryRows(taaf.getPayBeginDate(), taaf.getPayEndDate()));
-
-
-        return mapping.findForward("basic");
-    }
-
-    public ActionForward getMoreDocument(ActionMapping mapping, ActionForm form,
-                                         HttpServletRequest request, HttpServletResponse response) throws Exception {
-        TimeApprovalActionForm taaf = (TimeApprovalActionForm) form;
-        Criteria crit = new Criteria();
-        crit.addGreaterThan("documentId", taaf.getLastDocumentId());
-        List<TimesheetDocumentHeader> documentHeaders = TkServiceLocator.getTimesheetDocumentHeaderService().getSortedDocumentHeaders(taaf.getOrderBy(), taaf.getOrderDirection(), taaf.getRows());
-
-        String outputHtml = "";
-        for (TimesheetDocumentHeader documentHeader : documentHeaders) {
-            outputHtml += "<tr>" +
-                    "<td><span id='" + documentHeader.getDocumentId() + "' class='document'>" + documentHeader.getDocumentId() + "</span></td>" +
-                    "<td>" + documentHeader.getPrincipalId() + "</td>" +
-                    "<td><label for='checkbox'><input type='checkbox'/></label>" +
-                    "</tr>";
+        StringBuilder outputHtml = new StringBuilder();
+        for (ApprovalTimeSummaryRow row : taaf.getApprovalRows()) {
+            outputHtml.append("<tr>");
+            outputHtml.append("<td><span id='").append(row.getDocumentId()).append("' class='document'>").append(row.getDocumentId());
+            outputHtml.append("</span></td>");
+            outputHtml.append("<td>").append(row.getName()).append("</td>");
+            outputHtml.append("<td><label for='checkbox'><input type='checkbox'/></label></tr>");
         }
-
-        taaf.setOutputString(outputHtml);
+        taaf.setOutputString(outputHtml.toString());
 
         return mapping.findForward("ws");
     }
 
-    public ActionForward searchDocumentHeaders(ActionMapping mapping, ActionForm form,
-                                               HttpServletRequest request, HttpServletResponse response) throws Exception {
-
+    public ActionForward searchDocumentHeaders(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
         TimeApprovalActionForm taaf = (TimeApprovalActionForm) form;
         List<String> results = TkServiceLocator.getTimesheetDocumentHeaderService().getValueByField(taaf.getSearchField(), taaf.getTerm().toLowerCase());
         taaf.setOutputString(JSONValue.toJSONString(results));
 
         return mapping.findForward("ws");
-
     }
 
-    public ActionForward getSortedDocumentHeaders(ActionMapping mapping, ActionForm form,
-                                               HttpServletRequest request, HttpServletResponse response) throws Exception {
+    /**
+     * Helper method to modify / manage the list of records needed to display approval data to the user.
+     * @param sortTerm
+     * @param ascending
+     * @param rowsToReturn
+     * @param beginDate
+     * @param endDate
+     * @return
+     */
+    List<ApprovalTimeSummaryRow> getApprovalRows(String sortTerm, boolean ascending, int rowsToReturn, Date beginDate, Date endDate) {
+        List<ApprovalTimeSummaryRow> rows = new ArrayList<ApprovalTimeSummaryRow>();
 
-        TimeApprovalActionForm taaf = (TimeApprovalActionForm) form;
-        String orderBy = "";
-        List<TimesheetDocumentHeader> documentHeaders = TkServiceLocator.getTimesheetDocumentHeaderService().getSortedDocumentHeaders(taaf.getOrderBy(), taaf.getOrderDirection(), taaf.getRows());
-        taaf.setDocHeaders(documentHeaders);
+        // TODO : Fill this in.
 
-        return mapping.findForward("basic");
+        return rows;
     }
 
 }
--- src/main/java/org/kuali/hr/time/approval/web/TimeApprovalActionForm.java
+++ src/main/java/org/kuali/hr/time/approval/web/TimeApprovalActionForm.java
@@ -1,7 +1,6 @@
 package org.kuali.hr.time.approval.web;
 
 import org.kuali.hr.time.base.web.TkForm;
-import org.kuali.hr.time.workflow.TimesheetDocumentHeader;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,29 +8,36 @@ import java.util.List;
 
 public class TimeApprovalActionForm extends TkForm {
 
-    /**
-     *
-     */
+    public static final String ORDER_BY_PRINCIPAL = "principalId";
+    public static final String ORDER_BY_DOCID = "documentId";
+
     private static final long serialVersionUID = -173408280988754540L;
 
     private String name;
-    private List<ApprovalTimeSummaryRow> approvalTimeSummaryRows = new ArrayList<ApprovalTimeSummaryRow>();
     private Date payBeginDate;
     private Date payEndDate;
     private List<String> payCalendarLabels = new ArrayList<String>();
-    private List<TimesheetDocumentHeader> docHeaders = new ArrayList<TimesheetDocumentHeader>();
     private List<ApprovalTimeSummaryRow> approvalRows = new ArrayList<ApprovalTimeSummaryRow>();
     private String lastDocumentId;
+
+    /** Used for ajax dynamic row updating */
     private String outputString;
+
     private String searchField;
     private String term;
-    private String rows;
-    private String orderBy;
-    private String orderDirection;
 
+    private int rows = 5;
+    private String orderBy = TimeApprovalActionForm.ORDER_BY_PRINCIPAL;
+    private boolean ascending = true;
+
+    /**
+     * Gets the name of the user that this row represents.
+     * @return String representing the users name.
+     */
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -47,26 +53,13 @@ public class TimeApprovalActionForm extends TkForm {
 	public Date getPayEndDate() {
 		return payEndDate;
 	}
-	public void setApprovalTimeSummaryRows(List<ApprovalTimeSummaryRow> approvalTimeSummaryRows) {
-		this.approvalTimeSummaryRows = approvalTimeSummaryRows;
-	}
-	public List<ApprovalTimeSummaryRow> getApprovalTimeSummaryRows() {
-		return approvalTimeSummaryRows;
-	}
+
 	public void setPayCalendarLabels(List<String> payCalendarLabels) {
 		this.payCalendarLabels = payCalendarLabels;
 	}
 	public List<String> getPayCalendarLabels() {
 		return payCalendarLabels;
 	}
-
-    public List<TimesheetDocumentHeader> getDocHeaders() {
-        return docHeaders;
-    }
-
-    public void setDocHeaders(List<TimesheetDocumentHeader> docHeaders) {
-        this.docHeaders = docHeaders;
-    }
 
     public String getLastDocumentId() {
         return lastDocumentId;
@@ -100,11 +93,11 @@ public class TimeApprovalActionForm extends TkForm {
         this.term = term;
     }
 
-    public String getRows() {
+    public int getRows() {
         return rows;
     }
 
-    public void setRows(String rows) {
+    public void setRows(int rows) {
         this.rows = rows;
     }
 
@@ -116,19 +109,19 @@ public class TimeApprovalActionForm extends TkForm {
         this.orderBy = orderBy;
     }
 
-    public String getOrderDirection() {
-        return orderDirection;
-    }
-
-    public void setOrderDirection(String orderDirection) {
-        this.orderDirection = orderDirection;
-    }
-
     public List<ApprovalTimeSummaryRow> getApprovalRows() {
         return approvalRows;
     }
 
     public void setApprovalRows(List<ApprovalTimeSummaryRow> approvalRows) {
         this.approvalRows = approvalRows;
+    }
+
+    public boolean isAscending() {
+        return ascending;
+    }
+
+    public void setAscending(boolean ascending) {
+        this.ascending = ascending;
     }
 }
