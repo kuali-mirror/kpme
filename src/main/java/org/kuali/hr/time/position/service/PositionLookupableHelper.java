309a4422dd30ba9b6727d3ceb47430d0ef24ff8b
xichen <xichen@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-09-01 18:33:57 +0000

    KPME-938. Added fields to the inquiry pages.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1790 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/position/service/PositionLookupableHelper.java
+++ src/main/java/org/kuali/hr/time/position/service/PositionLookupableHelper.java
@@ -1,6 +1,14 @@
 package org.kuali.hr.time.position.service;
 
+import java.util.List;
+
 import org.kuali.hr.time.HrEffectiveDateActiveLookupableHelper;
+import org.kuali.hr.time.department.Department;
+import org.kuali.hr.time.position.Position;
+import org.kuali.hr.time.principal.calendar.PrincipalCalendar;
+import org.kuali.hr.time.util.TKContext;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.lookup.HtmlData;
 
 public class PositionLookupableHelper extends HrEffectiveDateActiveLookupableHelper {
 
@@ -8,5 +16,38 @@ public class PositionLookupableHelper extends HrEffectiveDateActiveLookupableHel
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@SuppressWarnings("rawtypes")
+	@Override
+	public List<HtmlData> getCustomActionUrls(BusinessObject businessObject,
+			List pkNames) {
+		Position position = (Position) businessObject;
+		final Long hrPositionId = position.getHrPositionId();
+		final String className = this.getBusinessObjectClass().getName();
+		List<HtmlData> customActionUrls = super.getCustomActionUrls(
+				businessObject, pkNames);
+		if (TKContext.getUser().isSystemAdmin() ||
+				TKContext.getUser().isLocationAdmin()) {
+			HtmlData htmlData = new HtmlData() {
+
+				/**
+				 * 
+				 */
+				private static final long serialVersionUID = 3489101791881384392L;
+
+				@Override
+				public String constructCompleteHtmlTag() {
+					return "<a target=\"_blank\" href=\"inquiry.do?businessObjectClassName="
+							+ className
+							+ "&methodToCall=start&hrPositionId="
+							+ hrPositionId
+							+ "&positionNumber=\">view</a>";
+				}
+			};
+			customActionUrls.add(htmlData);
+		} else if (customActionUrls.size() != 0) {
+			customActionUrls.remove(0);
+		}
+		return customActionUrls;
+	}
 
 }
