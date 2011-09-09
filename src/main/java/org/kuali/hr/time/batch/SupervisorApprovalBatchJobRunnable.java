c9366d1745fcd0ff953d06492262695354c2a59d
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-09-09 12:46:49 +0000

    fixed supervisor approval job
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1819 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/batch/SupervisorApprovalBatchJobRunnable.java
+++ src/main/java/org/kuali/hr/time/batch/SupervisorApprovalBatchJobRunnable.java
@@ -1,17 +1,22 @@
 package org.kuali.hr.time.batch;
 
 import org.apache.log4j.Logger;
+import org.kuali.hr.time.service.base.TkServiceLocator;
+import org.kuali.hr.time.timesheet.TimesheetDocument;
 
 public class SupervisorApprovalBatchJobRunnable extends BatchJobEntryRunnable  {
 
-    private static final Logger LOG = Logger.getLogger(SupervisorApprovalBatchJobRunnable.class);
-
     public SupervisorApprovalBatchJobRunnable(BatchJobEntry entry) {
-        super(entry);
-    }
+		super(entry);
+	}
+
+	private static final Logger LOG = Logger.getLogger(SupervisorApprovalBatchJobRunnable.class);
 
     @Override
     public void doWork() throws Exception {
-
+		String principalId = batchJobEntry.getPrincipalId();
+		String documentId = batchJobEntry.getDocumentId();
+		TimesheetDocument timesheetDocument = TkServiceLocator.getTimesheetService().getTimesheetDocument(documentId);
+		TkServiceLocator.getTimesheetService().approveTimesheet(principalId, timesheetDocument);
     }
 }
