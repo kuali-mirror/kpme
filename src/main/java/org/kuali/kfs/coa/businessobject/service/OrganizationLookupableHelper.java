89a7cb9c7be75edb4d202bdcf917a89d11d95e7c
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-09-13 10:50:48 +0000

    KPME-560:npadharia:chart code tied with organization.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1833 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/kfs/coa/businessobject/service/OrganizationLookupableHelper.java
+++ src/main/java/org/kuali/kfs/coa/businessobject/service/OrganizationLookupableHelper.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.kuali.hr.time.util.TKContext;
 import org.kuali.kfs.coa.businessobject.Organization;
-import org.kuali.rice.kns.authorization.BusinessObjectRestrictions;
 import org.kuali.rice.kns.bo.BusinessObject;
 import org.kuali.rice.kns.lookup.HtmlData;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
-import org.kuali.rice.kns.web.struts.form.LookupForm;
 
 public class OrganizationLookupableHelper extends
 		KualiLookupableHelperServiceImpl {
@@ -46,19 +44,5 @@ public class OrganizationLookupableHelper extends
 		}
 		return customActionUrls;
 	}
-	
-	@SuppressWarnings("rawtypes")
-	@Override
-	public HtmlData getReturnUrl(BusinessObject businessObject,
-			LookupForm lookupForm, List returnKeys,
-			BusinessObjectRestrictions businessObjectRestrictions) {
-		if (lookupForm.getFieldConversions().containsKey("chartOfAccountsCode")) {
-			lookupForm.getFieldConversions().remove("chartOfAccountsCode");
-		}
-		if (returnKeys.contains("chartOfAccountsCode")) {
-			returnKeys.remove("chartOfAccountsCode");
-		}
-		return super.getReturnUrl(businessObject, lookupForm, returnKeys,
-				businessObjectRestrictions);
-	}
+	 
 }
