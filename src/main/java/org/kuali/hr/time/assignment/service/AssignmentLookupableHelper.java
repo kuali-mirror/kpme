f43cc91b5fdd185f5d87169f4fd493eec74f3175
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-09-22 20:52:39 +0000

    kpme-983: made search by department work for Assignment
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1890 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/assignment/service/AssignmentLookupableHelper.java
+++ src/main/java/org/kuali/hr/time/assignment/service/AssignmentLookupableHelper.java
@@ -1,9 +1,13 @@
 package org.kuali.hr.time.assignment.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.kuali.hr.job.Job;
 import org.kuali.hr.time.HrEffectiveDateActiveLookupableHelper;
 import org.kuali.hr.time.assignment.Assignment;
+import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.util.TKContext;
 import org.kuali.rice.kns.bo.BusinessObject;
 import org.kuali.rice.kns.lookup.HtmlData;
@@ -46,6 +50,38 @@ public class AssignmentLookupableHelper extends
 			customActionUrls.remove(0);
 		}
 		return customActionUrls;
+	}
+	
+	@SuppressWarnings({"unchecked" })
+	@Override
+	public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
+		
+		String deptName = "";
+		if(fieldValues.containsKey("dept") && !fieldValues.get("dept").isEmpty()) {
+			deptName = fieldValues.get("dept");
+			fieldValues.remove("dept");
+		}
+		List aList = super.getSearchResults(fieldValues);
+		if(deptName.isEmpty()) {
+			return aList;
+		}
+		List finalList = new ArrayList<Assignment>();
+		for(int i = 0; i< aList.size(); i++) {
+			Assignment anAssign = (Assignment) aList.get(i);
+			if(anAssign.getPrincipalId() != null && anAssign.getEffectiveDate() != null) {
+				List<Job> jobList = TkServiceLocator.getJobSerivce().getJobs(anAssign.getPrincipalId(), anAssign.getEffectiveDate());
+				for(Job aJob : jobList) {
+					if(!aJob.getJobNumber().equals(anAssign.getJobNumber())){
+						continue;
+					}				
+					if( aJob.getDept().equals(deptName)) {
+						finalList.add(anAssign);
+						break;
+					}
+				}
+			}
+		}
+		return finalList;
 	}
 
 
