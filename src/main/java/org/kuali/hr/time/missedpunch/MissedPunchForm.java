d5b170fc766776a57c502bf285467aec61fe2d8c
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-10-03 20:43:51 +0000

    KPME-776
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1952 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/missedpunch/MissedPunchForm.java
+++ src/main/java/org/kuali/hr/time/missedpunch/MissedPunchForm.java
@@ -1,17 +1,33 @@
 package org.kuali.hr.time.missedpunch;
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kns.util.ActionFormUtilMap;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.WebUtils;
 import org.kuali.rice.kns.web.struts.form.KualiTransactionalDocumentFormBase;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class MissedPunchForm extends KualiTransactionalDocumentFormBase {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     @Override
     public String getBackLocation() {
         return "Clock.do?methodToCall=closeMissedPunchDoc";
+    }
+
+    @Override
+    public void populate(HttpServletRequest request) {
+        super.populate(request);
+        ((ActionFormUtilMap) GlobalVariables.getKualiForm().getActionFormUtilMap()).setCacheValueFinderResults(false);
+
+        if (this.getMethodToCall() == null || StringUtils.isEmpty(this.getMethodToCall())) {
+            setMethodToCall(WebUtils.parseMethodToCall(this, request));
+        }
     }
 
 }
