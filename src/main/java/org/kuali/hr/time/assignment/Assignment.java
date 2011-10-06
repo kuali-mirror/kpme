13b96f8dbfa4c5098f03f91ed41eb63a742e4c9b
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-10-06 12:34:37 +0000

    KPME-1107 : npadharia : department defaulted form job and field made readonly on maint page.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@1956 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/assignment/Assignment.java
+++ src/main/java/org/kuali/hr/time/assignment/Assignment.java
@@ -170,17 +170,16 @@ public class Assignment extends HrBusinessObject {
 	}
 
 	public String getDept() {
-		//Commented as defaulting department on job tab is not effective.
-//		if(this.getJobNumber()!= null) {
-//			if(this.getJob() == null || !this.getJobNumber().equals(this.getJob().getJobNumber())) {
-//				if(this.getEffectiveDate()!=null){
-//					this.setJob(TkServiceLocator.getJobSerivce().getJob(this.getPrincipalId(), this.getJobNumber(), this.getEffectiveDate(), false));
-//				}else{
-//					this.setJob(TkServiceLocator.getJobSerivce().getJob(this.getPrincipalId(), this.getJobNumber(), TKUtils.getCurrentDate(), false));
-//				}
-//			}
-//			setDept((this.getJob() != null) ? this.getJob().getDept() : "");
-//		}
+		if(this.getJobNumber()!= null) {
+			if(this.getJob() == null || !this.getJobNumber().equals(this.getJob().getJobNumber())) {
+				if(this.getEffectiveDate()!=null){
+					this.setJob(TkServiceLocator.getJobSerivce().getJob(this.getPrincipalId(), this.getJobNumber(), this.getEffectiveDate(), false));
+				}else{
+					this.setJob(TkServiceLocator.getJobSerivce().getJob(this.getPrincipalId(), this.getJobNumber(), TKUtils.getCurrentDate(), false));
+				}
+			}
+			setDept((this.getJob() != null) ? this.getJob().getDept() : "");
+		}
 		return dept;
 	}
 
