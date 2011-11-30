9abbc5063b129f7d2eb5a5501d254ec00596cfe0
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-11-30 16:51:27 +0000

    fixed user service
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1.1-br@2161 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/user/service/UserService.java
+++ src/main/java/org/kuali/hr/time/user/service/UserService.java
@@ -1,12 +1,11 @@
 package org.kuali.hr.time.user.service;
 
-import java.sql.Date;
-
 import org.kuali.hr.time.util.TKUser;
 import org.kuali.rice.kim.bo.Person;
+
+import java.sql.Date;
 
 public interface UserService {
     public TKUser buildTkUser(String actualPrincipalId, Date asOfDate);
     public TKUser buildTkUser(Person actual, Person backdoor, Person target, Date asOfDate);
-    public void loadRoles(TKUser tkUser);
 }
