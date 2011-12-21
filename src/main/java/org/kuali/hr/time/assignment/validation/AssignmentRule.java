8fd41b43f596b57c5277991519416c67a05965ab
henrydfs <henrydfs@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-12-21 15:01:04 +0000

    KPME-1182 ========= Modified Files: ApplicationResources.properties AssignmentRule.java
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2233 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/assignment/validation/AssignmentRule.java
+++ src/main/java/org/kuali/hr/time/assignment/validation/AssignmentRule.java
@@ -61,17 +61,17 @@ public class AssignmentRule extends MaintenanceDocumentRuleBase {
 	protected boolean validateDepartment(Assignment assignment) {
 		boolean valid = true;
 		if (assignment.getDept() != null) {
-				Criteria crit = new Criteria();
-				crit.addEqualTo("dept", assignment.getDept());
-				crit.addEqualTo("jobNumber", assignment.getJobNumber());
-				Query query = QueryFactory.newQuery(Job.class, crit);
-				int count = PersistenceBrokerFactory.defaultPersistenceBroker()
-						.getCount(query);
-				valid = (count > 0);
-				if (!valid) {
-					this.putFieldError("dept", "dept.jobnumber.invalid.sync");
-				}
-			 
+			Criteria crit = new Criteria();
+			crit.addEqualTo("dept", assignment.getDept());
+			crit.addEqualTo("jobNumber", assignment.getJobNumber());
+			Query query = QueryFactory.newQuery(Job.class, crit);
+			int count = PersistenceBrokerFactory.defaultPersistenceBroker()
+					.getCount(query);
+			valid = (count > 0);
+			if (!valid) {
+				this.putFieldError("dept", "dept.jobnumber.invalid.sync");
+			}
+
 		}
 		return valid;
 	}
@@ -155,22 +155,35 @@ public class AssignmentRule extends MaintenanceDocumentRuleBase {
 		}
 		return valid;
 	}
-	
+
 	protected boolean validateRegPayEarnCode(Assignment assignment) {
 		boolean valid = true;
 		int index = 0;
-		LOG.debug("Validating Regular pay EarnCodes: " + assignment.getAssignmentAccounts().size());
-		for(AssignmentAccount assignmentAccount : assignment.getAssignmentAccounts()){
-			if(assignment.getJobNumber()!=null && assignment.getPrincipalId()!=null){
-				Job job = TkServiceLocator.getJobSerivce().getJob(assignment.getPrincipalId(), assignment.getJobNumber(), assignment.getEffectiveDate(), false);
-				if(job !=null){
-					PayType payType = TkServiceLocator.getPayTypeSerivce().getPayType(job.getHrPayType(), assignment.getEffectiveDate());
-					if(!StringUtils.equals(assignmentAccount.getEarnCode(), payType.getRegEarnCode())){
-						valid = false;
-						this.putFieldError("assignmentAccounts[" + index
-								+ "].earnCode","earncode.regular.pay.required", assignmentAccount.getEarnCode());
+		LOG.debug("Validating Regular pay EarnCodes: "
+				+ assignment.getAssignmentAccounts().size());
+		for (AssignmentAccount assignmentAccount : assignment
+				.getAssignmentAccounts()) {
+			if (assignment.getJobNumber() != null
+					&& assignment.getPrincipalId() != null) {
+				Job job = TkServiceLocator.getJobSerivce().getJob(
+						assignment.getPrincipalId(), assignment.getJobNumber(),
+						assignment.getEffectiveDate(), false);
+				if (job != null) {
+					PayType payType = TkServiceLocator.getPayTypeSerivce()
+							.getPayType(job.getHrPayType(),
+									assignment.getEffectiveDate());
+					if (!StringUtils.equals(assignmentAccount.getEarnCode(),
+							payType.getRegEarnCode())) {
+						List <String> nowValidOvertimeCodes = TkServiceLocator.getEarnCodeService().getOvertimeEarnCodesStrs(assignment.getEffectiveDate());
+						if (!nowValidOvertimeCodes.contains(assignmentAccount.getEarnCode())) {
+							valid = false;
+							this.putFieldError("assignmentAccounts[" + index
+									+ "].earnCode",
+									"earncode.valid.pay.required",
+									assignmentAccount.getEarnCode());
+						}
 					}
-					
+
 				}
 			}
 			index++;
@@ -215,8 +228,8 @@ public class AssignmentRule extends MaintenanceDocumentRuleBase {
 				+ assignmentAccount.getFinSubObjCd());
 		if (assignmentAccount.getFinSubObjCd() != null) {
 			Map<String, String> fields = new HashMap<String, String>();
-			fields.put("financialSubObjectCode", assignmentAccount
-					.getFinSubObjCd());
+			fields.put("financialSubObjectCode",
+					assignmentAccount.getFinSubObjCd());
 			Collection subObjectCode = KNSServiceLocator.getBusinessObjectDao()
 					.findMatching(SubObjectCode.class, fields);
 			valid = subObjectCode.size() > 0;
@@ -229,27 +242,31 @@ public class AssignmentRule extends MaintenanceDocumentRuleBase {
 		}
 		return valid;
 	}
-	
-	protected boolean validateHasAccounts(Assignment assign){
-		if(assign.getAssignmentAccounts().isEmpty()){
+
+	protected boolean validateHasAccounts(Assignment assign) {
+		if (assign.getAssignmentAccounts().isEmpty()) {
 			this.putGlobalError("error.assign.must.have.one.or.more.account");
 			return false;
 		}
 		return true;
 	}
-	
-	protected boolean validateActiveFlag(Assignment assign){
-		if(!assign.isActive()) {
-			List<TimeBlock> tbList = TkServiceLocator.getTimeBlockService().getTimeBlocksForAssignment(assign);
-			if(!tbList.isEmpty()) {
+
+	protected boolean validateActiveFlag(Assignment assign) {
+		if (!assign.isActive()) {
+			List<TimeBlock> tbList = TkServiceLocator.getTimeBlockService()
+					.getTimeBlocksForAssignment(assign);
+			if (!tbList.isEmpty()) {
 				Date tbEndDate = tbList.get(0).getEndDate();
-				for(TimeBlock tb : tbList) {
-					if(tb.getEndDate().after(tbEndDate)) {
-						tbEndDate = tb.getEndDate();			// get the max end date
+				for (TimeBlock tb : tbList) {
+					if (tb.getEndDate().after(tbEndDate)) {
+						tbEndDate = tb.getEndDate(); // get the max end date
 					}
 				}
-				if(tbEndDate.equals(assign.getEffectiveDate()) || tbEndDate.after(assign.getEffectiveDate())) {
-					this.putFieldError("active", "error.assignment.timeblock.existence", tbEndDate.toString());
+				if (tbEndDate.equals(assign.getEffectiveDate())
+						|| tbEndDate.after(assign.getEffectiveDate())) {
+					this.putFieldError("active",
+							"error.assignment.timeblock.existence",
+							tbEndDate.toString());
 					return false;
 				}
 			}
