4376119f29b06bf8e5acb8ef0ad6f3cf50c0c20e
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-12-23 15:35:57 +0000

    KPME-1218
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1.1-br@2237 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/clocklog/service/ClockLogServiceImpl.java
+++ src/main/java/org/kuali/hr/time/clocklog/service/ClockLogServiceImpl.java
@@ -9,7 +9,6 @@ import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.task.Task;
 import org.kuali.hr.time.timeblock.TimeBlock;
 import org.kuali.hr.time.timesheet.TimesheetDocument;
-import org.kuali.hr.time.util.TKContext;
 import org.kuali.hr.time.util.TkConstants;
 
 import java.math.BigDecimal;
@@ -32,9 +31,9 @@ public class ClockLogServiceImpl implements ClockLogService {
     public ClockLog processClockLog(Timestamp clockTimeStamp, Assignment assignment, PayCalendarEntries pe, String ip, java.sql.Date asOfDate, TimesheetDocument td, String clockAction, String principalId) {
 
         // process rules
-        Timestamp clockTimestamp = TkServiceLocator.getGracePeriodService().processGracePeriodRule(clockTimeStamp, new java.sql.Date(pe.getBeginPeriodDateTime().getTime()));
+        Timestamp roundedClockTimestamp = TkServiceLocator.getGracePeriodService().processGracePeriodRule(clockTimeStamp, new java.sql.Date(pe.getBeginPeriodDateTime().getTime()));
 
-        ClockLog clockLog = TkServiceLocator.getClockLogService().buildClockLog(clockTimestamp, clockTimeStamp, assignment, td, clockAction, ip);
+        ClockLog clockLog = TkServiceLocator.getClockLogService().buildClockLog(roundedClockTimestamp, clockTimeStamp, assignment, td, clockAction, ip);
         TkServiceLocator.getClockLocationRuleService().processClockLocationRule(clockLog, asOfDate);
 
         // If the clock action is clock out or lunch out, create a time block besides the clock log
@@ -123,7 +122,7 @@ public class ClockLogServiceImpl implements ClockLogService {
         clockLog.setClockAction(clockAction);
         clockLog.setIpAddress(ip);
         clockLog.setHrJobId(assignment.getJob().getHrJobId());
-        clockLog.setUserPrincipalId(TKContext.getUser().getPrincipalId());
+        clockLog.setUserPrincipalId(principalId);
         // timestamp should be the original time without Grace Period rule applied
         clockLog.setTimestamp(originalTimestamp);
 
