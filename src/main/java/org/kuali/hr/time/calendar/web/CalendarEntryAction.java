a972cf8bc6242644eb31c64d13019062f2c2655e
npadharia <npadharia@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2011-12-29 08:15:43 +0000

    KPME-719 : npadharia : wrong or null calendar id restricted.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2261 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/calendar/web/CalendarEntryAction.java
+++ src/main/java/org/kuali/hr/time/calendar/web/CalendarEntryAction.java
@@ -17,29 +17,44 @@ public class CalendarEntryAction extends TkAction {
 			ActionForm form, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		CalendarEntryActionForm ceaf = (CalendarEntryActionForm) form;
-		if(ceaf.getNoOfPeriods()==null || ceaf.getNoOfPeriods() < 1){
-			GlobalVariables.getMessageMap().putError("document.noOfPeriods", "periods.greater.than.one");
+		if (ceaf.getNoOfPeriods() == null || ceaf.getNoOfPeriods() < 1) {
+			GlobalVariables.getMessageMap().putError("document.noOfPeriods",
+					"periods.greater.than.one");
+			return mapping.findForward("basic");
+		} else if (ceaf.getHrPyCalendarEntryId() == null) {
+			GlobalVariables.getMessageMap().putError(
+					"document.hrPyCalendarEntryId",
+					"error.calendar.not.available");
 			return mapping.findForward("basic");
 		}
 		CalendarEntries calendarEntries = TkServiceLocator
 				.getCalendarEntriesSerivce().getCalendarEntries(
 						ceaf.getHrPyCalendarEntryId());
-		CalendarEntries nextCalendarEntries = TkServiceLocator
-				.getCalendarEntriesSerivce()
-				.getNextCalendarEntriesByCalendarId(
-						calendarEntries.getHrCalendarId(), calendarEntries);
-		if (nextCalendarEntries != null) {
-			GlobalVariables.getMessageMap().putError("document.hrPyCalendarEntryId", "error.next.calendar.available");
+		if (calendarEntries == null) {
+			GlobalVariables.getMessageMap().putError(
+					"document.hrPyCalendarEntryId",
+					"error.calendar.not.available");
 		} else {
-			for (int i = 0; i < ceaf.getNoOfPeriods(); i++) {
-				TkServiceLocator.getCalendarEntriesSerivce()
-						.createNextCalendarEntry(calendarEntries);
-				calendarEntries = TkServiceLocator.getCalendarEntriesSerivce()
-						.getNextCalendarEntriesByCalendarId(
-								calendarEntries.getHrCalendarId(),
-								calendarEntries);
+			CalendarEntries nextCalendarEntries = TkServiceLocator
+					.getCalendarEntriesSerivce()
+					.getNextCalendarEntriesByCalendarId(
+							calendarEntries.getHrCalendarId(), calendarEntries);
+			if (nextCalendarEntries != null) {
+				GlobalVariables.getMessageMap().putError(
+						"document.hrPyCalendarEntryId",
+						"error.next.calendar.available");
+			} else {
+				for (int i = 0; i < ceaf.getNoOfPeriods(); i++) {
+					TkServiceLocator.getCalendarEntriesSerivce()
+							.createNextCalendarEntry(calendarEntries);
+					calendarEntries = TkServiceLocator
+							.getCalendarEntriesSerivce()
+							.getNextCalendarEntriesByCalendarId(
+									calendarEntries.getHrCalendarId(),
+									calendarEntries);
+				}
+				ceaf.setMessage("Calendar entry sucessfully created.");
 			}
-			ceaf.setMessage("Calendar entry sucessfully created.");
 		}
 		return mapping.findForward("basic");
 	}
