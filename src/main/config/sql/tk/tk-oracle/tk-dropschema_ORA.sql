a25200bb70b39f36a0389e0dcd4398a3818bfec6
rdoshi <rdoshi@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-01-06 14:59:53 +0000

    SQL to drop oracle schema
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1.1-br@2308 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/main/config/sql/tk/tk-oracle/tk-dropschema_ORA.sql
@@ -0,0 +1,13 @@
+BEGIN 
+  FOR i IN (SELECT sequence_name FROM user_sequences) 
+    LOOP 
+      EXECUTE IMMEDIATE('DROP SEQUENCE ' || user || '.' || i.sequence_name); 
+    END LOOP; 
+    
+    FOR i IN (SELECT table_name FROM user_tables) 
+    LOOP 
+      EXECUTE IMMEDIATE('DROP TABLE ' || user || '.' || i.table_name || ' CASCADE CONSTRAINTS'); 
+    END LOOP; 
+END;
+
+
