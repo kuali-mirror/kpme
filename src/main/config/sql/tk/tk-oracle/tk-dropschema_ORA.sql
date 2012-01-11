296344fd2e03dc54eab59dbe536e09e50e18a401
lfox <lfox@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-01-11 19:38:15 +0000

    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1-2-br@2354 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/sql/tk/tk-oracle/tk-dropschema_ORA.sql
+++ src/main/config/sql/tk/tk-oracle/tk-dropschema_ORA.sql
@@ -1,13 +1,12 @@
-BEGIN 
-  FOR i IN (SELECT sequence_name FROM user_sequences) 
-    LOOP 
-      EXECUTE IMMEDIATE('DROP SEQUENCE ' || user || '.' || i.sequence_name); 
-    END LOOP; 
-    
-    FOR i IN (SELECT table_name FROM user_tables) 
-    LOOP 
-      EXECUTE IMMEDIATE('DROP TABLE ' || user || '.' || i.table_name || ' CASCADE CONSTRAINTS'); 
-    END LOOP; 
+BEGIN
+  FOR i IN (SELECT sequence_name FROM user_sequences)
+    LOOP
+      EXECUTE IMMEDIATE('DROP SEQUENCE ' || user || '.' || i.sequence_name);
+    END LOOP;
+ 
+    FOR i IN (SELECT table_name FROM user_tables)
+    LOOP
+      EXECUTE IMMEDIATE('DROP TABLE ' || user || '.' || i.table_name || ' CASCADE CONSTRAINTS');
+    END LOOP;
 END;
-
-
+/
