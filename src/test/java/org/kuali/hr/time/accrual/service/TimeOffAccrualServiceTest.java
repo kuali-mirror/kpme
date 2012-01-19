21adface413d799231d6ff41afe87ba7d472881b
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-01-19 20:05:02 +0000

    Fixes NPE, not test itself. Test needs more comments to indicate purpose of individual tests.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2379 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/time/accrual/service/TimeOffAccrualServiceTest.java
+++ src/test/java/org/kuali/hr/time/accrual/service/TimeOffAccrualServiceTest.java
@@ -1,19 +1,24 @@
 package org.kuali.hr.time.accrual.service;
 
+import java.sql.Date;
 import java.util.List;
 
+import org.joda.time.DateTime;
 import org.junit.Test;
 import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.test.TkTestCase;
 import org.kuali.hr.time.test.TkTestUtils;
 import org.kuali.hr.time.timesheet.TimesheetDocument;
 import org.kuali.hr.time.util.TKUtils;
+import org.kuali.hr.time.util.TkConstants;
 
 public class TimeOffAccrualServiceTest extends TkTestCase {
-	
+
 	@Test
 	public void testValidateAccrualHoursLimit() {
-		TimesheetDocument doc = TkTestUtils.populateTimesheetDocument(TKUtils.getCurrentDate());
+        // Should fix the date in time. Stops NPE. Date set here is arbitrary.
+        Date TEST_DATE = new Date((new DateTime(2011, 3, 1, 0, 0, 0, 0, TkConstants.SYSTEM_DATE_TIME_ZONE)).getMillis());
+		TimesheetDocument doc = TkTestUtils.populateTimesheetDocument(TEST_DATE);
 		List<String> warnings = TkServiceLocator.getTimeOffAccrualService().validateAccrualHoursLimit(doc);
 		assertTrue("There should be 1 warning message", warnings.size() == 1);
 		assertTrue("Warning message not right", warnings.toString().contains("Warning: Total hours entered (50.00) for Accrual Category RGN has exceeded balance (30.00)."));
