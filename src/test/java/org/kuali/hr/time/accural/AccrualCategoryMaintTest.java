8b1a2880a1af3f399ffbc5865e115367fd84c8ba
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-01-19 21:02:55 +0000

    Renamed and fixed test.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2382 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/test/java/org/kuali/hr/time/accural/AccrualCategoryMaintTest.java
@@ -0,0 +1,58 @@
+package org.kuali.hr.time.accural;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+import org.junit.Test;
+import org.kuali.hr.lm.accrual.AccrualCategory;
+import org.kuali.hr.time.test.HtmlUnitUtil;
+import org.kuali.hr.time.test.TkTestCase;
+import org.kuali.hr.time.test.TkTestConstants;
+import org.kuali.rice.kns.service.KNSServiceLocator;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class AccrualCategoryMaintTest extends TkTestCase {
+
+	private static final String TEST_CODE = "_T";
+	private static String accrualCategoryId;
+	private static final Date TEST_DATE = new Date(Calendar.getInstance().getTimeInMillis());
+	private static final Timestamp TEST_TIMESTAMP = new Timestamp(Calendar.getInstance().getTimeInMillis());
+
+	@Test
+	public void testAccrualCategoryMaint() throws Exception {
+		HtmlPage accuralCategoryLookup = HtmlUnitUtil.gotoPageAndLogin(TkTestConstants.Urls.ACCURAL_CATEGORY_MAINT_URL);
+		accuralCategoryLookup = HtmlUnitUtil.clickInputContainingText(accuralCategoryLookup, "search");
+		assertTrue("Page contains test AccuralCategory", accuralCategoryLookup.asText().contains(TEST_CODE.toString()));
+		HtmlPage maintPage = HtmlUnitUtil.clickAnchorContainingText(accuralCategoryLookup, "edit", accrualCategoryId.toString());
+		assertTrue("Maintenance Page contains test AccuralCategory", maintPage.asText().contains(TEST_CODE));
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		AccrualCategory accrualCategory = new AccrualCategory();
+		accrualCategory.setAccrualCategory(TEST_CODE);
+		accrualCategory.setActive(true);
+		accrualCategory.setDescr(TEST_CODE);
+		accrualCategory.setEffectiveDate(TEST_DATE);
+		accrualCategory.setTimestamp(TEST_TIMESTAMP);
+        accrualCategory.setLeavePlan("");
+        accrualCategory.setAccrualEarnInterval("");
+        accrualCategory.setUnitOfTime("");
+        accrualCategory.setPlanningMonths(1L);
+		KNSServiceLocator.getBusinessObjectService().save(accrualCategory);
+		accrualCategoryId = accrualCategory.getLmAccrualCategoryId();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		AccrualCategory accrualCategory = KNSServiceLocator
+				.getBusinessObjectService().findBySinglePrimaryKey(
+						AccrualCategory.class, accrualCategoryId);
+		KNSServiceLocator.getBusinessObjectService().delete(accrualCategory);
+		super.tearDown();
+	}
+
+}
--- src/test/java/org/kuali/hr/time/test/TkTestConstants.java
+++ src/test/java/org/kuali/hr/time/test/TkTestConstants.java
@@ -41,7 +41,7 @@ public final class TkTestConstants {
 		public static final String DEPT_LUNCH_RULE_MAINT_URL = BASE_URL + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.hr.time.dept.lunch.DeptLunchRule&returnLocation="+
 		BASE_URL + "/portal.do&hideReturnLink=true&docFormKey=88888888";
 
-		public static final String ACCURAL_CATEGORY_MAINT_URL = BASE_URL + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.hr.time.accrual.AccrualCategory&returnLocation="+
+		public static final String ACCURAL_CATEGORY_MAINT_URL = BASE_URL + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.hr.lm.accrual.AccrualCategory&returnLocation="+
 		BASE_URL + "/portal.do&hideReturnLink=true&docFormKey=88888888";
 
 		public static final String SAL_GROUP_MAINT_URL = BASE_URL + "/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.hr.time.salgroup.SalGroup&returnLocation="+
