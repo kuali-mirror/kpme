bac81ae824b1d30317472f5260e66c1599731b13
djunk <djunk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-01-23 16:30:43 +0000

    Fixed data assumptions for maintenance test.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2417 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/time/overtime/weekly/rule/WeeklyOvertimeRuleGroupMaintenanceTest.java
+++ src/test/java/org/kuali/hr/time/overtime/weekly/rule/WeeklyOvertimeRuleGroupMaintenanceTest.java
@@ -7,11 +7,11 @@ import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
+import org.springframework.web.util.HtmlUtils;
 
 
 public class WeeklyOvertimeRuleGroupMaintenanceTest extends WeeklyOvertimeRuleMaintenanceTest {
-	
+
 	protected static final String OVERTIME_EARN_GROUP="OVT";
 	@Test
 	//tests WeeklyOvertimeRuleValidation
@@ -19,30 +19,33 @@ public class WeeklyOvertimeRuleGroupMaintenanceTest extends WeeklyOvertimeRuleMa
     	String baseUrl = HtmlUnitUtil.getBaseURL() + "/kr/maintenance.do?businessObjectClassName=org.kuali.hr.time.overtime.weekly.rule.WeeklyOvertimeRuleGroup&methodToCall=edit";
     	HtmlPage page = HtmlUnitUtil.gotoPageAndLogin(baseUrl);
     	assertNotNull(page);
-   
+
     	HtmlForm form = page.getFormByName("KualiForm");
     	assertNotNull("Search form was missing from page.", form);
-    	
+
     	HtmlInput  input  = HtmlUnitUtil.getInputContainingText(form, "methodToCall.route");
     	assertNotNull("Could not locate submit button", input);
     	setFieldValue(page, "document.documentHeader.documentDescription", "test");
 		HtmlElement element = page.getElementByName("methodToCall.route");
         HtmlPage finalPage = element.click();
+        //HtmlUnitUtil.createTempFile(finalPage, "final");
 		assertTrue("Maintenance Page contains error messages",finalPage.asText().contains("The specified maxHoursEarnGroup '" + TEST_CODE + "' does not exist."));
 		assertTrue("Maintenance Page contains error messages",finalPage.asText().contains("The specified convertFromEarnGroup '" + TEST_CODE + "' does not exist."));
 		assertTrue("Maintenance Page contains error messages",finalPage.asText().contains("The specified convertToEarnCode '" + TEST_CODE + "' does not exist."));
-		
+
 		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.effectiveDate", "4/01/2011");
-		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.maxHoursEarnGroup", OVERTIME_EARN_GROUP);
-		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.convertFromEarnGroup", OVERTIME_EARN_GROUP);
+		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.maxHoursEarnGroup", "REG");
+		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.convertFromEarnGroup", "REG");
 		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.convertToEarnCode", OVERTIME_EARN_GROUP);
 		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.maxHours", "5");
 		setFieldValue(page, "document.newMaintainableObject.add.lstWeeklyOvertimeRules.step", "10");
-		
-		element = page.getElementByName("methodToCall.addLine.lstWeeklyOvertimeRules.(!!org.kuali.hr.time.overtime.weekly.rule.WeeklyOvertimeRule!!).(:::;1;:::).anchor1");
+        element = page.getElementById("methodToCall.addLine.lstWeeklyOvertimeRules.(!!org.kuali.hr.time.overtime.weekly.rule.WeeklyOvertimeRule!!)");
+        assertNotNull("Missing Add Line button", element);
+
         HtmlPage addPage = element.click();
+        //HtmlUnitUtil.createTempFile(addPage, "worgmt");
+
         assertTrue("Maintenance Page contains error messages",addPage.asText().contains("Rule already exists for step '10'."));
-        
 	}
 
 }
