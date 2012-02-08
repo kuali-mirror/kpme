c28df986100cde7cf4b75deff46c9c8e5183603a
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-02-08 22:09:55 +0000

    Changed EmployeeOverrideMaintTest to use sql for setup and cleanup
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2507 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/test/config/sql/EmployeeOverrideMaintTest-cleanup.sql
@@ -0,0 +1,3 @@
+delete from lm_employee_override_t where lm_employee_override_id = '3000';
+delete from lm_accrual_category_t where lm_accrual_category_id = '3000';
+delete from hr_principal_attributes_t where principal_id = '111';
--- src/test/java/org/kuali/hr/lm/employeeoverride/EmployeeOverrideMaintTest.java
+++ src/test/java/org/kuali/hr/lm/employeeoverride/EmployeeOverrideMaintTest.java
@@ -1,19 +1,9 @@
 package org.kuali.hr.lm.employeeoverride;
 
-import java.sql.Date;
-import java.sql.Timestamp;
-import java.util.Calendar;
-
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 import org.junit.Test;
-import org.kuali.hr.lm.accrual.AccrualCategory;
-import org.kuali.hr.time.principal.PrincipalHRAttributes;
-import org.kuali.hr.time.service.base.TkServiceLocator;
 import org.kuali.hr.time.test.HtmlUnitUtil;
 import org.kuali.hr.time.test.TkTestCase;
 import org.kuali.hr.time.test.TkTestConstants;
-import org.kuali.rice.kns.service.KNSServiceLocator;
 
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
@@ -21,10 +11,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class EmployeeOverrideMaintTest extends TkTestCase{
-	private static String eoId;
-	private static String acId;
 	private static final String PRINCIPAL_ID = "111";
-	private static final Date TEST_DATE = new Date((new DateTime(2011,1,1,1,0,0,0,DateTimeZone.forID("EST"))).getMillis());
 	private static final String EFFECTIVE_DATE_REQUIRED = "Effective Date (Effective Date) is a required field.";
 	private static final String PRINCIPAL_ID_REQUIRED = "Principal Id (Principal Id) is a required field.";
 	private static final String LEAVE_PLAN_REQUIRED = "Leave Plan (Leave Plan) is a required field.";
@@ -85,7 +72,6 @@ public class EmployeeOverrideMaintTest extends TkTestCase{
 	  	assertTrue("page text does not contain:\n" + AC_NOT_FOUND, page.asText().contains(AC_NOT_FOUND));
 	  	assertTrue("page text does not contain:\n" + LEAVE_PLAN_NOT_FOUND, page.asText().contains(LEAVE_PLAN_NOT_FOUND));
 	  	
-	  	
 	  	setFieldValue(page, "document.newMaintainableObject.accrualCategory", "testAC"); // existing accrual category
 	  	element = page.getElementByName("methodToCall.route");
 	  	page = element.click();
@@ -93,56 +79,10 @@ public class EmployeeOverrideMaintTest extends TkTestCase{
 	  	assertTrue("page text does not contain:\n" + LEAVE_PLAN_NOT_FOUND, page.asText().contains(LEAVE_PLAN_NOT_FOUND));
 	  	assertTrue("page text does not contain:\n" + AC_LP_INCONSISTENT, page.asText().contains(AC_LP_INCONSISTENT));
 	  	
-	  	
 	  	setFieldValue(page, "document.newMaintainableObject.principalId", PRINCIPAL_ID); // existing principal hr attributes
 	  	element = page.getElementByName("methodToCall.route");
 	  	page = element.click();
 	  	assertFalse("page text contains:\n" + LEAVE_PLAN_NOT_FOUND, page.asText().contains(LEAVE_PLAN_NOT_FOUND));
 	  	assertFalse("page text contains:\n" + AC_LP_INCONSISTENT, page.asText().contains(AC_LP_INCONSISTENT));
 	}
-	
-	@Override
-	public void setUp() throws Exception {
-		super.setUp();
-		EmployeeOverride eo = new EmployeeOverride();
-		eo.setPrincipalId(PRINCIPAL_ID);
-		eo.setEffectiveDate(TEST_DATE);
-		eo.setLeavePlan("testLP");
-		eo.setAccrualCategory("testAC");
-		eo.setOverrideType("Max Balance");
-		eo.setActive(true);
-		KNSServiceLocator.getBusinessObjectService().save(eo);
-		eoId = eo.getLmEmployeeOverrideId();
-		
-		AccrualCategory ac = new AccrualCategory();
-		ac.setAccrualCategory("testAC");
-		ac.setActive(true);
-		ac.setDescr("test");
-		ac.setEffectiveDate(TEST_DATE);
-		ac.setTimestamp(new Timestamp(Calendar.getInstance().getTimeInMillis()));
-		ac.setLeavePlan("testLP");
-		ac.setAccrualEarnInterval("");
-		ac.setUnitOfTime("");
-		KNSServiceLocator.getBusinessObjectService().save(ac);
-		acId = ac.getLmAccrualCategoryId();
-		
-		PrincipalHRAttributes pha = new PrincipalHRAttributes();
-		pha.setActive(true);
-		pha.setEffectiveDate(TEST_DATE);
-		pha.setPrincipalId(PRINCIPAL_ID);
-		pha.setLeavePlan("testLP");
-		KNSServiceLocator.getBusinessObjectService().save(pha);
-	}
-
-	@Override
-	public void tearDown() throws Exception {
-		EmployeeOverride eo = TkServiceLocator.getEmployeeOverrideService().getEmployeeOverride(eoId);
-		KNSServiceLocator.getBusinessObjectService().delete(eo);
-		AccrualCategory ac = TkServiceLocator.getAccrualCategoryService().getAccrualCategory(acId);
-		KNSServiceLocator.getBusinessObjectService().delete(ac);
-		PrincipalHRAttributes pha = TkServiceLocator.getPrincipalHRAttributeService().getPrincipalCalendar(PRINCIPAL_ID, TEST_DATE);
-		KNSServiceLocator.getBusinessObjectService().delete(pha);
-		super.tearDown();
-	}
-
 }
