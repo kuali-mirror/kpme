d189427e46de685ec5e2fd7cda356525017a2d4f
rdoshi <rdoshi@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-02-17 18:45:27 +0000

    Added caching
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1-2-br@2548 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/person/service/PersonServiceImpl.java
+++ src/main/java/org/kuali/hr/time/person/service/PersonServiceImpl.java
@@ -2,8 +2,10 @@ package org.kuali.hr.time.person.service;
 
 import java.util.List;
 
+import org.kuali.hr.time.cache.CacheResult;
 import org.kuali.hr.time.person.TKPerson;
 import org.kuali.hr.time.person.dao.PersonDao;
+import org.kuali.hr.time.util.TkConstants;
 
 
 public class PersonServiceImpl implements PersonService { 
@@ -14,6 +16,7 @@ public class PersonServiceImpl implements PersonService {
 	}
 
 	@Override
+	@CacheResult(secondsRefreshPeriod=TkConstants.DEFAULT_CACHE_TIME)
 	public List<TKPerson> getPersonCollection(List<String> principalIds) {
 		return personDao.getPersonCollection(principalIds);
 	}
