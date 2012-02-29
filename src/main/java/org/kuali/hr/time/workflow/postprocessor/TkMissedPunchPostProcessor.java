c97c43a288a96ed258ae3f19398f4b34b50e847d
rdoshi <rdoshi@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-02-29 20:29:17 +0000

    Commenting extra code
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-1-2-br@2613 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/workflow/postprocessor/TkMissedPunchPostProcessor.java
+++ src/main/java/org/kuali/hr/time/workflow/postprocessor/TkMissedPunchPostProcessor.java
@@ -3,8 +3,6 @@ package org.kuali.hr.time.workflow.postprocessor;
 import org.kuali.rice.kew.postprocessor.DefaultPostProcessor;
 import org.kuali.rice.kew.postprocessor.DocumentRouteStatusChange;
 import org.kuali.rice.kew.postprocessor.ProcessDocReport;
-import org.kuali.rice.kns.bo.DocumentHeader;
-import org.kuali.rice.kns.service.KNSServiceLocator;
 
 public class TkMissedPunchPostProcessor extends DefaultPostProcessor {
 
@@ -12,9 +10,9 @@ public class TkMissedPunchPostProcessor extends DefaultPostProcessor {
 	public ProcessDocReport doRouteStatusChange(DocumentRouteStatusChange statusChangeEvent) throws Exception {
 		ProcessDocReport pdr = super.doRouteStatusChange(statusChangeEvent);
 
-		Long documentId = statusChangeEvent.getRouteHeaderId();
+		/*Long documentId = statusChangeEvent.getRouteHeaderId();
 
-        DocumentHeader header = KNSServiceLocator.getDocumentHeaderService().getDocumentHeaderById(documentId.toString());
+        DocumentHeader header = KNSServiceLocator.getDocumentHeaderService().getDocumentHeaderById(documentId.toString());*/
 		return pdr;
 	}
 
