2c8d1ccc2f4023ce2249834083050071595f18a0
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-02-29 14:01:30 +0000

    KPME-1344 Message added in Application resources, for valid Calendar start for Leave Plan Document and changed test case according KPME -1345
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2610 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/java/org/kuali/hr/lm/leaveplan/LeavePlanMaintTest.java
+++ src/test/java/org/kuali/hr/lm/leaveplan/LeavePlanMaintTest.java
@@ -13,19 +13,25 @@ public class LeavePlanMaintTest extends TkTestCase {
 
 	public static final String TEST_USER = "admin";
 	
+	
+	
 	@Test
 	public void testLeavePlanMonthsSave() throws Exception {
 		
 		//get the page with planning months
 		HtmlPage leavePlan = HtmlUnitUtil.gotoPageAndLogin(TkTestConstants.Urls.LEAVE_PLAN_MAINT_URL);
+		System.out.println("Leave plan text page is : "+leavePlan.asText());
 		HtmlPage resultPage = HtmlUnitUtil.clickInputContainingText(leavePlan, "search");
 		HtmlUnitUtil.createTempFile(resultPage);
+		System.out.println("Result page is : "+resultPage.asText());
 		assertTrue("Maintenance page contains:\n" + "Testing Leave Plan Months", resultPage.asText().contains("Testing Leave Plan Months"));
 		
 		//submit a leave plan with planning months changed
 		HtmlPage leavePlanMaintPage = HtmlUnitUtil.clickAnchorContainingText(resultPage, "edit", "5555");
 		setFieldValue(leavePlanMaintPage, "document.documentHeader.documentDescription", "LeavePlan change planningMonths - test");
+		setFieldValue(leavePlanMaintPage, "document.newMaintainableObject.effectiveDate", "01/01/2013");
 		HtmlInput planningMonthsText = HtmlUnitUtil.getInputContainingText(leavePlanMaintPage, "document.newMaintainableObject.planningMonths");
+		
 		planningMonthsText.setValueAttribute("999"); //max value
 		HtmlPage outputPage = HtmlUnitUtil.clickInputContainingText(leavePlanMaintPage, "submit");
 		HtmlUnitUtil.createTempFile(outputPage);
