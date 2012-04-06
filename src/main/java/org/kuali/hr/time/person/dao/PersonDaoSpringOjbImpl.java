38be31ab90806f0d4df3528b0d03fadfc13cacbb
lee55 <lee55@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-04-06 15:15:30 +0000

    fix the issue on the approval tab when the person object is nul
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2717 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/person/dao/PersonDaoSpringOjbImpl.java
+++ src/main/java/org/kuali/hr/time/person/dao/PersonDaoSpringOjbImpl.java
@@ -9,24 +9,26 @@ import org.springmodules.orm.ojb.support.PersistenceBrokerDaoSupport;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PersonDaoSpringOjbImpl extends PersistenceBrokerDaoSupport  implements PersonDao {
+public class PersonDaoSpringOjbImpl extends PersistenceBrokerDaoSupport implements PersonDao {
 
-	private static final Logger LOG = Logger.getLogger(PersonDaoSpringOjbImpl.class);
-	
-	public List<TKPerson> getPersonCollection(List<String> principalIds) {
-		
-		List<TKPerson> persons = new LinkedList<TKPerson>();
-		
-		for(String principalId: principalIds){
-			Person p = KIMServiceLocator.getPersonService().getPerson(principalId);
-			TKPerson person = new TKPerson();
-			person.setPrincipalId(p.getPrincipalId());
-			person.setFirstName(p.getFirstName());
-			person.setLastName(p.getLastName());
-			person.setPrincipalName(p.getName());
-			persons.add(person);
-		}
-		return persons;
-	}
+    private static final Logger LOG = Logger.getLogger(PersonDaoSpringOjbImpl.class);
+
+    public List<TKPerson> getPersonCollection(List<String> principalIds) {
+
+        List<TKPerson> persons = new LinkedList<TKPerson>();
+
+        for (String principalId : principalIds) {
+            Person p = KIMServiceLocator.getPersonService().getPerson(principalId);
+            if (p != null) {
+                TKPerson person = new TKPerson();
+                person.setPrincipalId(p.getPrincipalId());
+                person.setFirstName(p.getFirstName());
+                person.setLastName(p.getLastName());
+                person.setPrincipalName(p.getName());
+                persons.add(person);
+            }
+        }
+        return persons;
+    }
 
 }
