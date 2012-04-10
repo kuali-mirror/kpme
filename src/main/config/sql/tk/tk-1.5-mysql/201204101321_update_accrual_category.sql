31fc878303228eb1332cb2a2fa280f25dfc8896b
tkagata <tkagata@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-04-10 17:41:43 +0000

    KPME-1347
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2734 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/sql/tk/tk-1.5-mysql/201204101321_update_accrual_category.sql
+++ src/main/config/sql/tk/tk-1.5-mysql/201204101321_update_accrual_category.sql
@@ -1,4 +1,2 @@
-alter table lm_accrual_category_t modify column ACCRUAL_INTERVAL_EARN varchar(1) NOT NULL DEFAULT "M";
-alter table lm_accrual_category_t drop column PLANNING_MONTHS;
 alter table lm_accrual_category_t modify column DESCR varchar(50);
 alter table lm_accrual_category_t add column LEAVE_CODE VARCHAR(15);
