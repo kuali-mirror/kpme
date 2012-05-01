344017c82b709d917f359fc83aa420c6a7269bba
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-05-01 12:50:32 +0000

    KPME-1431 Leave Plan validation done for Leave Adjustment.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2780 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/lm/leavecode/service/LeaveCodeServiceImpl.java
+++ src/main/java/org/kuali/hr/lm/leavecode/service/LeaveCodeServiceImpl.java
@@ -23,74 +23,80 @@ import com.google.common.collect.Ordering;
 
 public class LeaveCodeServiceImpl implements LeaveCodeService {
 
-    private static final Logger LOG = Logger.getLogger(LeaveCodeServiceImpl.class);
-    private LeaveCodeDao leaveCodeDao;
+	private static final Logger LOG = Logger
+			.getLogger(LeaveCodeServiceImpl.class);
+	private LeaveCodeDao leaveCodeDao;
 
+	public LeaveCodeDao getLeaveCodeDao() {
+		return leaveCodeDao;
+	}
 
-    public LeaveCodeDao getLeaveCodeDao() {
-        return leaveCodeDao;
-    }
+	public void setLeaveCodeDao(LeaveCodeDao leaveCodeDao) {
+		this.leaveCodeDao = leaveCodeDao;
+	}
 
+	@Override
+	@CacheResult(secondsRefreshPeriod = TkConstants.DEFAULT_CACHE_TIME)
+	public LeaveCode getLeaveCode(String lmLeaveCodeId) {
+		return getLeaveCodeDao().getLeaveCode(lmLeaveCodeId);
+	}
 
-    public void setLeaveCodeDao(LeaveCodeDao leaveCodeDao) {
-        this.leaveCodeDao = leaveCodeDao;
-    }
-
-
-    @Override
-    @CacheResult(secondsRefreshPeriod = TkConstants.DEFAULT_CACHE_TIME)
-    public LeaveCode getLeaveCode(String lmLeaveCodeId) {
-        return getLeaveCodeDao().getLeaveCode(lmLeaveCodeId);
-    }
-
-    @Override
+	@Override
     public List<LeaveCode> getLeaveCodes(String principalId, Date asOfDate) {
+    	String leavePlan = null;
         List<LeaveCode> leaveCodes = new ArrayList<LeaveCode>();
         PrincipalHRAttributes hrAttribute = TkServiceLocator.getPrincipalHRAttributeService().getPrincipalCalendar(principalId, asOfDate);
-        String leavePlan = hrAttribute.getLeavePlan();
-        if (StringUtils.isBlank(leavePlan)) {
-            throw new RuntimeException("No leave plan defined for " + principalId + " in principal hr attributes");
-        }
+        if(hrAttribute != null) {
+        	leavePlan = hrAttribute.getLeavePlan();
+        	if (StringUtils.isBlank(leavePlan)) {
+        		throw new RuntimeException("No leave plan defined for " + principalId + " in principal hr attributes");
+        	}
+        	
+            List<LeaveCode> unfilteredLeaveCodes = leaveCodeDao.getLeaveCodes(leavePlan, asOfDate);
+            TKUser user = TKContext.getUser();
 
-        List<LeaveCode> unfilteredLeaveCodes = leaveCodeDao.getLeaveCodes(leavePlan, asOfDate);
-        TKUser user = TKContext.getUser();
-
-        for (LeaveCode leaveCode : unfilteredLeaveCodes) {
-            //if employee add this leave code
-            //TODO how do we know this is an approver for them
-            if ((leaveCode.getEmployee() && user.getCurrentRoles().isActiveEmployee()) ||
-                    (leaveCode.getApprover() && user.isApprover())) {
-                leaveCodes.add(leaveCode);
+            for (LeaveCode leaveCode : unfilteredLeaveCodes) {
+                //if employee add this leave code
+                //TODO how do we know this is an approver for them
+                if ((leaveCode.getEmployee() && user.getCurrentRoles().isActiveEmployee()) ||
+                        (leaveCode.getApprover() && user.isApprover())) {
+                    leaveCodes.add(leaveCode);
+                }
             }
+
         }
+
         return leaveCodes;
     }
 
-    @Override
-    @CacheResult(secondsRefreshPeriod = TkConstants.DEFAULT_CACHE_TIME)
-    public Map<String, String> getLeaveCodesForDisplay(String principalId) {
-        List<LeaveCode> leaveCodes = getLeaveCodes(principalId, TKUtils.getCurrentDate());
+	@Override
+	@CacheResult(secondsRefreshPeriod = TkConstants.DEFAULT_CACHE_TIME)
+	public Map<String, String> getLeaveCodesForDisplay(String principalId) {
+		List<LeaveCode> leaveCodes = getLeaveCodes(principalId,
+				TKUtils.getCurrentDate());
 
-        Comparator<LeaveCode> leaveCodeComparator = new Comparator<LeaveCode>() {
-            @Override
-            public int compare(LeaveCode leaveCode, LeaveCode leaveCode1) {
-                return leaveCode.getLeaveCode().compareToIgnoreCase(leaveCode1.getLeaveCode());
-            }
-        };
-        // Order by leaveCode ascending
-        Ordering<LeaveCode> ordering = Ordering.from(leaveCodeComparator);
+		Comparator<LeaveCode> leaveCodeComparator = new Comparator<LeaveCode>() {
+			@Override
+			public int compare(LeaveCode leaveCode, LeaveCode leaveCode1) {
+				return leaveCode.getLeaveCode().compareToIgnoreCase(
+						leaveCode1.getLeaveCode());
+			}
+		};
+		// Order by leaveCode ascending
+		Ordering<LeaveCode> ordering = Ordering.from(leaveCodeComparator);
 
-        Map<String, String> leaveCodesForDisplay = new LinkedHashMap<String, String>();
-        for (LeaveCode leaveCode : ordering.sortedCopy(leaveCodes)) {
-            leaveCodesForDisplay.put(leaveCode.getLeaveCodeKeyForDisplay(), leaveCode.getLeaveCodeValueForDisplay());
-        }
+		Map<String, String> leaveCodesForDisplay = new LinkedHashMap<String, String>();
+		for (LeaveCode leaveCode : ordering.sortedCopy(leaveCodes)) {
+			leaveCodesForDisplay.put(leaveCode.getLeaveCodeKeyForDisplay(),
+					leaveCode.getLeaveCodeValueForDisplay());
+		}
 
-        return leaveCodesForDisplay;
-    }
+		return leaveCodesForDisplay;
+	}
 
-    @Override
-    @CacheResult(secondsRefreshPeriod=TkConstants.DEFAULT_CACHE_TIME)
-    public LeaveCode getLeaveCode(String leaveCode, Date effectiveDate){
-    	return leaveCodeDao.getLeaveCode(leaveCode, effectiveDate);
-    }
+	@Override
+	@CacheResult(secondsRefreshPeriod = TkConstants.DEFAULT_CACHE_TIME)
+	public LeaveCode getLeaveCode(String leaveCode, Date effectiveDate) {
+		return leaveCodeDao.getLeaveCode(leaveCode, effectiveDate);
+	}
 }
