66431223046a6a3c7416201b89ee6278e3269939
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-05-18 16:34:53 +0000

    kpme-1528: created inquirableImpl classes for AccrualCategory and leavePlan to make inquiry of leavePlan work from AccrualCategory search results, added two brand new tests
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2835 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/test/config/sql/LeavePlanInquirableImplTest-cleanup.sql
@@ -0,0 +1,3 @@
+delete from lm_leave_plan_t where LM_LEAVE_PLAN_ID = '5000';
+delete from lm_leave_plan_t where LM_LEAVE_PLAN_ID = '5001';
+delete from lm_leave_plan_t where LM_LEAVE_PLAN_ID = '5002';
---
+++ src/test/java/org/kuali/hr/lm/accrualcategory/validation/AccrualCategoryInquirableImplTest.java
@@ -0,0 +1,31 @@
+package org.kuali.hr.lm.accrualcategory.validation;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+import org.kuali.hr.lm.accrual.AccrualCategory;
+import org.kuali.hr.lm.accrual.AccrualCategoryInquirableImpl;
+import org.kuali.hr.time.test.TkTestCase;
+import org.kuali.rice.kns.lookup.HtmlData;
+import org.kuali.rice.kns.lookup.HtmlData.AnchorHtmlData;
+
+public class AccrualCategoryInquirableImplTest extends TkTestCase {
+
+	private String INQUIRY_URL = "inquiry.do?businessObjectClassName=org.kuali.hr.lm.leaveplan.LeavePlan&lmLeavePlanId=&methodToCall=start&effectiveDate=03%2F04%2F2012&leavePlan=testLeavePlan";
+	@Test
+	public void testGetInquiryUrl() throws Exception {
+		AccrualCategory ac = new AccrualCategory();
+		ac.setAccrualCategory("testAC");
+		ac.setLeavePlan("testLeavePlan");
+		Date aDate = new java.sql.Date(new SimpleDateFormat("MM/dd/yyyy").parse("03/04/2012").getTime());
+		ac.setEffectiveDate(aDate);
+		
+		HtmlData hd = (HtmlData) new AccrualCategoryInquirableImpl().getInquiryUrl(ac, "leavePlan", false);
+		assertNotNull("No HtmlData found", hd);
+		String inquiryUrl = ((AnchorHtmlData) hd).getHref();
+		assertTrue("Inquiry url is wrong", StringUtils.contains(inquiryUrl, INQUIRY_URL));
+		
+	}
+}
