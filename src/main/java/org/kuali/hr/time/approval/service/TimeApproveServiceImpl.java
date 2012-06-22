393c0f4e2f727428732799e106a4e07bd2186fff
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-06-22 13:19:45 +0000

    KPME-1660 Approve tab prb solved
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@2878 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/time/approval/service/TimeApproveServiceImpl.java
+++ src/main/java/org/kuali/hr/time/approval/service/TimeApproveServiceImpl.java
@@ -636,7 +636,7 @@ public class TimeApproveServiceImpl implements TimeApproveService {
 	    } else {
 	      List<String> principalIds = new ArrayList<String>();
 	      SqlRowSet rs = null;
-          sql = "SELECT DISTINCT A0.PRINCIPAL_ID FROM TK_ASSIGNMENT_T A0, HR_ROLES_T R0, TK_WORK_AREA_T W0, HR_PRINCIPAL_CALENDAR_T P0  WHERE "
+          sql = "SELECT DISTINCT A0.PRINCIPAL_ID FROM TK_ASSIGNMENT_T A0, HR_ROLES_T R0, TK_WORK_AREA_T W0, HR_PRINCIPAL_ATTRIBUTES_T P0  WHERE "
         		  + "((A0.EFFDT =  (SELECT MAX(EFFDT)  FROM TK_ASSIGNMENT_T  WHERE PRINCIPAL_ID = A0.PRINCIPAL_ID  AND EFFDT <= ? AND WORK_AREA = A0.WORK_AREA  AND TASK = A0.TASK AND JOB_NUMBER = A0.JOB_NUMBER) AND "
                   + "A0.TIMESTAMP =  (SELECT MAX(TIMESTAMP)  FROM TK_ASSIGNMENT_T  WHERE PRINCIPAL_ID = A0.PRINCIPAL_ID  AND EFFDT = A0.EFFDT AND WORK_AREA = A0.WORK_AREA AND TASK = A0.TASK AND JOB_NUMBER = A0.JOB_NUMBER) AND "
                   + "A0.ACTIVE = 'Y') OR (A0.ACTIVE = 'N'  AND A0.EFFDT >= ? AND A0.EFFDT <= ?)) AND "
@@ -657,7 +657,7 @@ public class TimeApproveServiceImpl implements TimeApproveService {
                   + "W0.TIMESTAMP =  (SELECT MAX(TIMESTAMP)  FROM TK_WORK_AREA_T  WHERE WORK_AREA = W0.WORK_AREA  AND EFFDT = W0.EFFDT) AND "
                   + "W0.ACTIVE = 'Y' AND "
                   + "P0.PRINCIPAL_ID = A0.PRINCIPAL_ID AND "
-                  + "P0.PY_CALENDAR_GROUP = ?";
+                  + "P0.PAY_CALENDAR = ?";
 
 
 	       int[] params = null;
