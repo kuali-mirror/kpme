2c2eae371ba60c71fcac7ce9e65752a2d937a5fe
kbtaylor <kbtaylor@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-07-19 15:42:00 +0000

    Substituting ; for / delimiters in mysql
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/version-rice-2-0-br@2963 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/config/db/rice/2.0.0/mysql/update_client_final_mysql.sql
+++ src/main/config/db/rice/2.0.0/mysql/update_client_final_mysql.sql
@@ -20,9 +20,9 @@
 
 
 ALTER TABLE krns_sesn_doc_t ADD (OBJ_ID VARCHAR(36))
-/
+;
 ALTER TABLE krns_sesn_doc_t ADD (VER_NBR DECIMAL(8) DEFAULT 0)
-/
+;
 
 
 
@@ -33,7 +33,7 @@ ALTER TABLE krns_sesn_doc_t ADD (VER_NBR DECIMAL(8) DEFAULT 0)
 
 
 ALTER TABLE KRSB_MSG_QUE_T CHANGE SVC_NMSPC APPL_ID VARCHAR(255)
-/
+;
 
 
 
