f2ca64fdfe79dca168ee33dfe9a12945752fd4e4
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-08-14 20:39:39 +0000

    kpme-1680: fixed unit test
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@3079 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/config/sql/LeaveDonationMaintTest-cleanup.sql
+++ src/test/config/sql/LeaveDonationMaintTest-cleanup.sql
@@ -3,5 +3,5 @@ delete from hr_principal_attributes_t where hr_principal_attribute_id >= '5000';
 delete from lm_leave_plan_t where lm_leave_plan_id >= '8000';
 delete from lm_accrual_category_t where lm_accrual_category_id >= '5000';
 delete from hr_earn_code_t where hr_earn_code_id >= '5000';
-delete from lm_leave_block_t where principal_id like('testUser%');
-delete from lm_leave_block_hist_t where principal_id like('testUser%');
+delete from lm_leave_block_t where principal_id in('10001', '10003');
+delete from lm_leave_block_hist_t where principal_id in('10001', '10003');
