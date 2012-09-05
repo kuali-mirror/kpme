3a94de2d6a55a6cdefd8a3636266905a6b46c4f0
jjhanso <jjhanso@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-09-05 16:20:31 +0000

    KPME-1749 - several tables missing primary key constraint
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@3172 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/config/sql/ActualTimeInquiryWebTest.sql
+++ src/test/config/sql/ActualTimeInquiryWebTest.sql
@@ -3,3 +3,5 @@ insert into hr_calendar_entries_t (hr_calendar_entry_id,hr_calendar_id, calendar
 
 delete from tk_document_header_t where document_id = '2';
 insert into tk_document_header_t values ('2', 'admin', ADDDATE(now(),14), 'I', now(), NULL, '1');
+
+update tk_grace_period_rl_t set active = 'Y' where tk_grace_period_rule_id = '1';
