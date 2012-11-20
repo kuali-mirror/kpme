b153a67161760253aa8afbe402db2a4f0bf0dc10
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-11-20 05:10:02 +0000

    Automated license header update
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@3748 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/test/config/sql/PrincipalHRAttributeServiceTest.sql
+++ src/test/config/sql/PrincipalHRAttributeServiceTest.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2004-2012 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 delete from hr_principal_attributes_t where principal_id like('testUser%');
 
 insert into hr_principal_attributes_t (`hr_principal_attribute_id`, `principal_id`, `pay_calendar`, `leave_plan`, `service_date`, `fmla_eligible`, `workers_eligible`, `timezone`, `EFFDT`, `TIMESTAMP`, `OBJ_ID`, `VER_NBR`, `active`, `leave_calendar`) values('5001', 'testUser', 'BWS-CAL', 'testLP', '2012-03-01', 'Y', 'Y', null, '2012-03-01', '2012-03-01 00:00:00', uuid(), '1', 'Y', 'LM');
