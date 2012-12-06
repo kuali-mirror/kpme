5f0878406999992b437b8c4c5f4d47d6a68e4bf9
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2012-12-06 06:32:34 +0000

    KPME-1947 Leave Plan cannot exist more than once if active for same time period
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@3788 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/lm/leaveplan/validation/LeavePlanValidation.java
+++ src/main/java/org/kuali/hr/lm/leaveplan/validation/LeavePlanValidation.java
@@ -24,90 +24,114 @@ import org.apache.commons.lang.StringUtils;
 import org.kuali.hr.lm.leaveplan.LeavePlan;
 import org.kuali.hr.time.principal.PrincipalHRAttributes;
 import org.kuali.hr.time.service.base.TkServiceLocator;
+import org.kuali.hr.time.util.TKUtils;
 import org.kuali.hr.time.util.ValidationUtils;
 import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
 import org.kuali.rice.krad.bo.PersistableBusinessObject;
 
+public class LeavePlanValidation extends MaintenanceDocumentRuleBase {
 
-public class LeavePlanValidation extends MaintenanceDocumentRuleBase {	 
-	
-	//KPME-1250 Kagata
+	// KPME-1250 Kagata
 	// This method determines if the leave plan can be inactivated
-	boolean validateInactivation(LeavePlan leavePlan){
+	boolean validateInactivation(LeavePlan leavePlan) {
 		boolean valid = true;
-		// Get a list of active employees based on leave plan and its effective date.
-		// If the list is not null, there are active employees and the leave plan can't be inactivated, so return false otherwise true
-		if(!leavePlan.isActive()) {
-			//this has to use the effective date of the job passed in
-			List<PrincipalHRAttributes> pList = TkServiceLocator.getPrincipalHRAttributeService().getActiveEmployeesForLeavePlan(leavePlan.getLeavePlan(), leavePlan.getEffectiveDate());
-			
+		// Get a list of active employees based on leave plan and its effective
+		// date.
+		// If the list is not null, there are active employees and the leave
+		// plan can't be inactivated, so return false otherwise true
+		if (!leavePlan.isActive()) {
+			// this has to use the effective date of the job passed in
+			List<PrincipalHRAttributes> pList = TkServiceLocator
+					.getPrincipalHRAttributeService()
+					.getActiveEmployeesForLeavePlan(leavePlan.getLeavePlan(),
+							leavePlan.getEffectiveDate());
+
 			if (pList != null && pList.size() > 0) {
-				// error.leaveplan.inactivate=Can not inactivate leave plan {0}.  There are active employees in the plan.
-				this.putFieldError("active", "error.leaveplan.inactivate", leavePlan.getLeavePlan());
+				// error.leaveplan.inactivate=Can not inactivate leave plan {0}.
+				// There are active employees in the plan.
+				this.putFieldError("active", "error.leaveplan.inactivate",
+						leavePlan.getLeavePlan());
 				valid = false;
-			} 
+			}
 		}
 
 		return valid;
 	}
-	
+
 	// KPME-1407 Kagata
 	boolean validatePlanningMonths(String planningMonths) {
 		boolean valid = true;
-		if (planningMonths != null ){
+		if (planningMonths != null) {
 			int iPlanningMonths = Integer.parseInt(planningMonths);
 			// error.leaveplan.planningMonths='{0}' should be between 1 and 24.
-			if (iPlanningMonths > 24  || iPlanningMonths <= 0) {
-				this.putFieldError("planningMonths", "error.leaveplan.planningMonths", "Planning Months");
+			if (iPlanningMonths > 24 || iPlanningMonths <= 0) {
+				this.putFieldError("planningMonths",
+						"error.leaveplan.planningMonths", "Planning Months");
 				valid = false;
 			}
 		}
 		return valid;
 	}
-	
-	boolean validateEffectiveDate(Date effectiveDate) {
+
+	boolean validateEffectiveDate(Date effectiveDate, String leavePlan) {
 		boolean valid = true;
 		valid = ValidationUtils.validateOneYearFutureEffectiveDate(effectiveDate);
 		if(!valid) {
 			this.putFieldError("effectiveDate", "error.date.exceed.year", "Effective Date");
+		} else {
+			if(leavePlan != null && StringUtils.isNotEmpty(leavePlan.trim())) {
+		        LeavePlan lastLeavePlan = TkServiceLocator.getLeavePlanService().getLeavePlan(leavePlan,effectiveDate);
+		        if(lastLeavePlan != null) {
+			        if(TKUtils.getTimelessDate(lastLeavePlan.getEffectiveDate()).compareTo(TKUtils.getTimelessDate(effectiveDate)) <= 0){
+			        	valid = false;
+			        	this.putFieldError("effectiveDate", "error.leavePlan.effectiveDate.newr.exists", "Effective Date");
+			        }
+		        }
+			}
 		}
 		return valid;
 	}
-	
+
 	boolean validateCalendarYearStart(String dateString) {
-        if (StringUtils.isBlank(dateString)) {
-            return false;
-        }
+		if (StringUtils.isBlank(dateString)) {
+			return false;
+		}
 		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd");
 		sdf.setLenient(false);
 		try {
 			sdf.parse(dateString);
 		} catch (ParseException e) {
-			this.putFieldError("calendarYearStart", "error.calendar.year.start", "Calendar Year Start");
+			this.putFieldError("calendarYearStart",
+					"error.calendar.year.start", "Calendar Year Start");
 			return false;
 		}
 		return true;
 	}
-	
+
 	@Override
 	protected boolean processCustomRouteDocumentBusinessRules(
 			MaintenanceDocument document) {
 		boolean valid = true;
 		LOG.debug("entering custom validation for Leave Plan");
-		PersistableBusinessObject pbo = (PersistableBusinessObject) this.getNewBo();
+		PersistableBusinessObject pbo = (PersistableBusinessObject) this
+				.getNewBo();
 		if (pbo instanceof LeavePlan) {
 			LeavePlan leavePlan = (LeavePlan) pbo;
 			if (leavePlan != null) {
 				valid = true;
 				valid &= this.validateInactivation(leavePlan);
-				if(StringUtils.isNotEmpty(leavePlan.getPlanningMonths())) {
-					valid &= this.validatePlanningMonths(leavePlan.getPlanningMonths());
+				if (StringUtils.isNotEmpty(leavePlan.getPlanningMonths())) {
+					valid &= this.validatePlanningMonths(leavePlan
+							.getPlanningMonths());
 				}
-				if(leavePlan.getEffectiveDate() != null) {
-					valid &= this.validateEffectiveDate(leavePlan.getEffectiveDate());
-				}			
-				valid &= this.validateCalendarYearStart(leavePlan.getCalendarYearStart());
+				if (leavePlan.getEffectiveDate() != null) {
+					valid &= this.validateEffectiveDate(
+							leavePlan.getEffectiveDate(),
+							leavePlan.getLeavePlan());
+				}
+				valid &= this.validateCalendarYearStart(leavePlan
+						.getCalendarYearStart());
 			}
 		}
 		return valid;
