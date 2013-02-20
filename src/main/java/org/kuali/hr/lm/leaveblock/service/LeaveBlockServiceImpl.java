b82dd6215b6ef0a508714a7c72791774f49ddfe7
jjhanso <jjhanso@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-02-20 22:13:28 +0000

    KPME-2174 - can't add leave block from time detail on last day of calendar entry
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@4208 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- src/main/java/org/kuali/hr/lm/leaveblock/service/LeaveBlockServiceImpl.java
+++ src/main/java/org/kuali/hr/lm/leaveblock/service/LeaveBlockServiceImpl.java
@@ -167,7 +167,7 @@ public class LeaveBlockServiceImpl implements LeaveBlockService {
        
         // To create the correct interval by the given begin and end dates,
         // we need to plus one day on the end date to include that date
-        List<Interval> leaveBlockIntervals = TKUtils.createDaySpan(beginDate, endDate.plusDays(1), TKUtils.getSystemDateTimeZone());
+        List<Interval> leaveBlockIntervals = TKUtils.createDaySpan(beginDate.toDateMidnight().toDateTime(), endDate.plusDays(1).toDateMidnight().toDateTime(), TKUtils.getSystemDateTimeZone());
         // need to use beginDate and endDate of the calendar to find all leaveBlocks since LeaveCalendarDocument Id is not always available
         List<LeaveBlock> currentLeaveBlocks = getLeaveBlocks(principalId, calBeginDateTime.toDate(), calEndDateTime.toDate());
     
