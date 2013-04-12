970eae7416981208e604d0dce8219b4143fd9d12
yingzhou <yingzhou@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-04-12 22:02:47 +0000

    kpme-2286: added position qualification value
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/kpme-2.0@4467 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ src/main/java/org/kuali/hr/pm/pstnqlfctnvl/dao/PositionQualificationValueDao.java
@@ -0,0 +1,8 @@
+package org.kuali.hr.pm.pstnqlfctnvl.dao;
+
+import org.kuali.hr.pm.pstnqlfctnvl.PositionQualificationValue;
+
+public interface PositionQualificationValueDao {
+	public PositionQualificationValue getPositionQualificationValueByValue(String value);
+
+}
---
+++ src/main/java/org/kuali/hr/pm/pstnqlfctnvl/validation/PositionQualificationValueValidation.java
@@ -0,0 +1,7 @@
+package org.kuali.hr.pm.pstnqlfctnvl.validation;
+
+import org.kuali.rice.krad.rules.MaintenanceDocumentRuleBase;
+
+public class PositionQualificationValueValidation extends MaintenanceDocumentRuleBase {
+
+}
