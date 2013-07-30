81dff9eae9ad3cbfab6e86195fb6800320a5d588
drgodfrey <drgodfrey@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-07-30 21:06:48 +0000

    KPME-2601: Adding rule skeleton. KPME-2589: Adding rule skeleton. KPME-2597: Adding rule skeleton. KPME-2600: Adding rule skeleton. KPME-2599: Adding rule skeleton.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5164 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ core/impl/src/main/java/org/kuali/kpme/core/kfs/coa/businessobject/validation/AccountValidation.java
@@ -0,0 +1,22 @@
+package org.kuali.kpme.core.kfs.coa.businessobject.validation;
+
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+
+@SuppressWarnings("deprecation")
+public class AccountValidation extends MaintenanceDocumentRuleBase {
+
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(
+			MaintenanceDocument document) {
+		// TODO Auto-generated method stub
+		return super.processCustomRouteDocumentBusinessRules(document);
+	}
+	
+	@Override
+	protected boolean validateMaintenanceDocument(
+			MaintenanceDocument maintenanceDocument) {
+		return super.validateMaintenanceDocument(maintenanceDocument);
+	}
+
+}
---
+++ core/impl/src/main/java/org/kuali/kpme/core/kfs/coa/businessobject/validation/ChartValidation.java
@@ -0,0 +1,23 @@
+package org.kuali.kpme.core.kfs.coa.businessobject.validation;
+
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+
+@SuppressWarnings("deprecation")
+public class ChartValidation extends MaintenanceDocumentRuleBase {
+
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(
+			MaintenanceDocument document) {
+		// TODO Auto-generated method stub
+		return super.processCustomRouteDocumentBusinessRules(document);
+	}
+
+	@Override
+	protected boolean validateMaintenanceDocument(
+			MaintenanceDocument maintenanceDocument) {
+		// TODO Auto-generated method stub
+		return super.validateMaintenanceDocument(maintenanceDocument);
+	}
+
+}
