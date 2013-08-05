a90f7888020baca0c813c1a996ebb48b6541915c
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-08-05 06:46:40 +0000

    KPME-2612 - Department Lunch Deduction Rule Maint Doc issues
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5194 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ core/impl/src/main/java/org/kuali/kpme/core/job/web/JobInquirableImpl.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2004-2013 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.core.job.web;
+
+
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.LocalDate;
+import org.kuali.kpme.core.department.Department;
+import org.kuali.kpme.core.job.Job;
+import org.kuali.kpme.core.service.HrServiceLocator;
+import org.kuali.kpme.core.util.TKUtils;
+import org.kuali.rice.kns.inquiry.KualiInquirableImpl;
+import org.kuali.rice.krad.bo.BusinessObject;
+
+public class JobInquirableImpl extends KualiInquirableImpl {
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public BusinessObject getBusinessObject(Map fieldValues) {
+        Job jobObj = null;
+
+        if (StringUtils.isNotBlank((String) fieldValues.get("hrJobId"))) {
+            jobObj = HrServiceLocator.getJobService().getJob((String) fieldValues.get("hrJobId"));
+        } else if (fieldValues.containsKey("jobNumber") && fieldValues.containsKey("effectiveDate") && fieldValues.containsKey("principalId")) {
+        	String jobNumberVal = (String) fieldValues.get("jobNumber");
+        	Long jobNumber = jobNumberVal != null ? Long.valueOf(jobNumberVal) : null;
+            String principalId = (String) fieldValues.get("principalId");
+            LocalDate effectiveDate = TKUtils.formatDateString((String) fieldValues.get("effectiveDate"));
+            jobObj = HrServiceLocator.getJobService().getJob(principalId, jobNumber, effectiveDate);
+        } else {
+            jobObj = (Job) super.getBusinessObject(fieldValues);
+        }
+
+        return jobObj;
+    }
+}
