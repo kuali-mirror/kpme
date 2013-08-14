94459a332edd27f1a39bbbb34dacbc79d1a5ed3c
drgodfrey <drgodfrey@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-08-14 12:40:09 +0000

    KPME-2685: fix time entry format bug
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5276 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- web/src/main/webapp/js/tk.js
+++ web/src/main/webapp/js/tk.js
@@ -88,7 +88,7 @@ var CONSTANTS = {
     },
     TIME_FORMAT : {
         DATE_FOR_OUTPUT : 'M/d/yyyy',
-        TIME_FOR_VALIDATION: ['ht', 'h tt', 'h:mmt', 'h:mm tt', 'hh:mmt', 'hh:mm tt', 'Hmm', 'HHmm', 'H:mm', 'HH:mm'],
+        TIME_FOR_VALIDATION: ['hh:mm tt', 'h:mm tt', 'hh:mmt', 'h:mmt', 'HH:mm', 'HHmm', 'H:mm', 'h tt', 'Hmm', 'ht'],
         TIME_FOR_OUTPUT : 'hh:mm tt',
         TIME_FOR_SYSTEM : 'H:mm'
     }
