3a190187a3421846083239547fac30b5939e81a6
smoaddel <smoaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-08-16 20:52:12 +0000

    added check for duplicate location  and a required active role(KPME-2696)
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5296 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ core/impl/src/main/java/org/kuali/kpme/core/location/validation/LocationValidation.java
@@ -0,0 +1,86 @@
+package org.kuali.kpme.core.location.validation;
+
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.LocalDate;
+import org.kuali.kpme.core.department.Department;
+import org.kuali.kpme.core.location.Location;
+import org.kuali.kpme.core.role.KPMERole;
+import org.kuali.kpme.core.role.location.LocationPrincipalRoleMemberBo;
+import org.kuali.kpme.core.service.HrServiceLocator;
+import org.kuali.kpme.core.util.TKUtils;
+import org.kuali.rice.kim.api.role.Role;
+import org.kuali.rice.kim.api.services.KimApiServiceLocator;
+import org.kuali.rice.kim.impl.role.RoleMemberBo;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.krad.bo.PersistableBusinessObject;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class LocationValidation extends MaintenanceDocumentRuleBase {
+
+    @Override
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+
+        boolean valid = true;
+
+        PersistableBusinessObject pbo = (PersistableBusinessObject) this.getNewBo();
+
+        if (pbo instanceof Location) {
+            Location location = (Location) pbo;
+            valid &= validateLocation(location);
+            valid &= validateRolePresent(location.getRoleMembers(), location.getEffectiveLocalDate());
+        }
+
+        return valid;
+    }
+
+    protected boolean validateLocation(Location newLocation) {
+        boolean valid = true;
+
+        if (newLocation.getLocation() != null && newLocation.getEffectiveDate() != null) {
+            List<Location> locations = HrServiceLocator.getLocationService().getLocations(newLocation.getLocation());
+            if (locations != null && locations.size() > 0) {
+                for(Location location : locations) {
+                    if(!location.getHrLocationId().equalsIgnoreCase(newLocation.getHrLocationId())) {
+                        this.putFieldError("location", "error.location.duplicate.exists", location.getLocation());
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return valid;
+    }
+
+    boolean validateRolePresent(List<LocationPrincipalRoleMemberBo> roleMembers, LocalDate effectiveDate) {
+        boolean valid = true;
+        boolean activeFlag = false;
+
+        for (ListIterator<LocationPrincipalRoleMemberBo> iterator = roleMembers.listIterator(); iterator.hasNext(); ) {
+            int index = iterator.nextIndex();
+            RoleMemberBo roleMember = iterator.next();
+            Role role = KimApiServiceLocator.getRoleService().getRole(roleMember.getRoleId());
+            activeFlag |= roleMember.isActive();
+
+            String prefix = "roleMembers[" + index + "].";
+
+            if (roleMember.getActiveToDateValue() != null) {
+                if (effectiveDate.compareTo(roleMember.getActiveToDate().toLocalDate()) >= 0
+                        || roleMember.getActiveFromDateValue().compareTo(roleMember.getActiveToDateValue()) >= 0) {
+                    this.putFieldError(prefix + "expirationDate", "error.role.expiration");
+                    valid = false;
+                }
+            }
+        }
+
+        if (!activeFlag) {
+            this.putGlobalError("role.required");
+        }
+
+        return valid & activeFlag;
+    }
+
+}
