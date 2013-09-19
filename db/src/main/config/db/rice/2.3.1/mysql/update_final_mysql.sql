3ebb3f0f8d4ecd259eceea8b89642e5ed6fd8af4
jjhanso <jjhanso@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-09-19 17:32:35 +0000

    KPME-2817 - updating rice 2.3.1 mysql upgrade script to be upper case
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5553 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- db/src/main/config/db/rice/2.3.1/mysql/update_final_mysql.sql
+++ db/src/main/config/db/rice/2.3.1/mysql/update_final_mysql.sql
@@ -18,181 +18,181 @@
 -- KULRICE-9288  - Column 'SESN_ID' cannot be null causes issues
 --
 
-DELETE FROM krim_perm_attr_data_t
+DELETE FROM KRIM_PERM_ATTR_DATA_T
 WHERE perm_id =
       (
         SELECT
-          DISTINCT a.perm_id
-        FROM krim_perm_t a, krim_perm_tmpl_t b
-        WHERE a.perm_tmpl_id = b.perm_tmpl_id AND b.nmspc_cd = 'KR-NS' AND b.nm = 'Edit Document' AND a.nmspc_cd = 'KUALI'
-              AND a.nm = 'Edit Kuali ENROUTE Document Node Name PreRoute'
+          DISTINCT a.PERM_ID
+        FROM KRIM_PERM_T a, KRIM_PERM_TMPL_T b
+        WHERE a.PERM_TMPL_ID = b.PERM_TMPL_ID AND b.NMSPC_CD = 'KR-NS' AND b.NM = 'Edit Document' AND a.NMSPC_CD = 'KUALI'
+              AND a.NM = 'Edit Kuali ENROUTE Document Node Name PreRoute'
       )
 ;
 
-DELETE FROM krim_role_perm_t
-WHERE perm_id =
+DELETE FROM KRIM_ROLE_PERM_T
+WHERE PERM_ID =
       (
         SELECT
-          DISTINCT a.perm_id
-        FROM krim_perm_t a, krim_perm_tmpl_t b
-        WHERE a.perm_tmpl_id = b.perm_tmpl_id AND b.nmspc_cd = 'KR-NS' AND b.nm = 'Edit Document' AND a.nmspc_cd = 'KUALI'
-              AND a.nm = 'Edit Kuali ENROUTE Document Node Name PreRoute'
+          DISTINCT a.PERM_ID
+        FROM KRIM_PERM_T a, KRIM_PERM_TMPL_T b
+        WHERE a.PERM_TMPL_ID = b.PERM_TMPL_ID AND b.NMSPC_CD = 'KR-NS' AND b.NM = 'Edit Document' AND a.NMSPC_CD = 'KUALI'
+              AND a.NM = 'Edit Kuali ENROUTE Document Node Name PreRoute'
       )
 ;
 
-DELETE FROM krim_perm_t
-WHERE nmspc_cd = 'KUALI' AND nm = 'Edit Kuali ENROUTE Document Node Name PreRoute' AND perm_tmpl_id =
+DELETE FROM KRIM_PERM_T
+WHERE NMSPC_CD = 'KUALI' AND NM = 'Edit Kuali ENROUTE Document Node Name PreRoute' AND PERM_TMPL_ID =
                                                                                        (
                                                                                          SELECT
-                                                                                           perm_tmpl_id
-                                                                                         FROM krim_perm_tmpl_t
-                                                                                         WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+                                                                                           PERM_TMPL_ID
+                                                                                         FROM KRIM_PERM_TMPL_T
+                                                                                         WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
                                                                                        )
 ;
 
-INSERT INTO krim_perm_t (perm_id, obj_id, ver_nbr, perm_tmpl_id, nmspc_cd, nm, desc_txt, actv_ind)
+INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND)
   VALUES (
     'KR1001', uuid(), 1,
     (
       SELECT
-        perm_tmpl_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        PERM_TMPL_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     'KUALI', 'Edit Kuali ENROUTE Document Route Status Code I',
     'Allows users to edit Kuali documents that are in INITIATED status.', 'Y'
   )
 ;
 
-INSERT INTO krim_perm_attr_data_t (attr_data_id, obj_id, ver_nbr, perm_id, kim_typ_id, kim_attr_defn_id, attr_val)
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL)
   VALUES (
     'KR1001', uuid(), 1, 'KR1001',
     (
       SELECT
-        kim_typ_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        KIM_TYP_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     (
       SELECT
-        kim_attr_defn_id
-      FROM krim_attr_defn_t
-      WHERE nmspc_cd = 'KR-WKFLW' AND nm = 'documentTypeName'
+        KIM_ATTR_DEFN_ID
+      FROM KRIM_ATTR_DEFN_T
+      WHERE NMSPC_CD = 'KR-WKFLW' AND NM = 'documentTypeName'
     ),
     'KualiDocument'
   )
 ;
 
-INSERT INTO krim_perm_attr_data_t (attr_data_id, obj_id, ver_nbr, perm_id, kim_typ_id, kim_attr_defn_id, attr_val)
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL)
   VALUES (
     'KR1002', uuid(), 1, 'KR1001',
     (
       SELECT
-        kim_typ_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        KIM_TYP_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     (
       SELECT
-        kim_attr_defn_id
-      FROM krim_attr_defn_t
-      WHERE nmspc_cd = 'KR-WKFLW' AND nm = 'routeStatusCode'
+        KIM_ATTR_DEFN_ID
+      FROM KRIM_ATTR_DEFN_T
+      WHERE NMSPC_CD = 'KR-WKFLW' AND NM = 'routeStatusCode'
     ),
     'I'
   )
 ;
 
-INSERT INTO krim_role_perm_t (role_perm_id, obj_id, ver_nbr, role_id, perm_id, actv_ind)
+INSERT INTO KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND)
   VALUES (
     'KR1002', uuid(), 1,
     (
       SELECT
-        role_id
-      FROM krim_role_t
-      WHERE role_nm = 'Initiator' AND nmspc_cd = 'KR-WKFLW'
+        ROLE_ID
+      FROM KRIM_ROLE_T
+      WHERE ROLE_NM = 'Initiator' AND NMSPC_CD = 'KR-WKFLW'
     ),
     (
       SELECT
-        perm_id
-      FROM krim_perm_t
-      WHERE nmspc_cd = 'KUALI' AND nm = 'Edit Kuali ENROUTE Document Route Status Code I'
+        PERM_ID
+      FROM KRIM_PERM_T
+      WHERE NMSPC_CD = 'KUALI' AND NM = 'Edit Kuali ENROUTE Document Route Status Code I'
     ),
     'Y'
   )
 ;
 
-INSERT INTO krim_perm_t (perm_id, obj_id, ver_nbr, perm_tmpl_id, nmspc_cd, nm, desc_txt, actv_ind)
+INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND)
   VALUES (
     'KR1002', uuid(), 1,
     (
       SELECT
-        perm_tmpl_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        PERM_TMPL_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     'KUALI', 'Edit Kuali ENROUTE Document Route Status Code S',
     'Allows users to edit Kuali documents that are in SAVED status.', 'Y'
   )
 ;
 
-INSERT INTO krim_perm_attr_data_t (attr_data_id, obj_id, ver_nbr, perm_id, kim_typ_id, kim_attr_defn_id, attr_val)
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL)
   VALUES (
     'KR1003', uuid(), 1, 'KR1002',
     (
       SELECT
-        kim_typ_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        KIM_TYP_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     (
       SELECT
-        kim_attr_defn_id
-      FROM krim_attr_defn_t
-      WHERE nmspc_cd = 'KR-WKFLW' AND nm = 'documentTypeName'
+        KIM_ATTR_DEFN_ID
+      FROM KRIM_ATTR_DEFN_T
+      WHERE NMSPC_CD = 'KR-WKFLW' AND NM = 'documentTypeName'
     ), 'KualiDocument'
   )
 ;
 
-INSERT INTO krim_perm_attr_data_t (attr_data_id, obj_id, ver_nbr, perm_id, kim_typ_id, kim_attr_defn_id, attr_val)
+INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_TYP_ID, KIM_ATTR_DEFN_ID, ATTR_VAL)
   VALUES (
     'KR1004', uuid(), 1,'KR1002',
     (
       SELECT
-        kim_typ_id
-      FROM krim_perm_tmpl_t
-      WHERE nmspc_cd = 'KR-NS' AND nm = 'Edit Document'
+        KIM_TYP_ID
+      FROM KRIM_PERM_TMPL_T
+      WHERE NMSPC_CD = 'KR-NS' AND NM = 'Edit Document'
     ),
     (
       SELECT
-        kim_attr_defn_id
-      FROM krim_attr_defn_t
-      WHERE nmspc_cd = 'KR-WKFLW' AND nm = 'routeStatusCode'
+        KIM_ATTR_DEFN_ID
+      FROM KRIM_ATTR_DEFN_T
+      WHERE NMSPC_CD = 'KR-WKFLW' AND NM = 'routeStatusCode'
     ),
     'S'
   )
 ;
 
-INSERT INTO krim_role_perm_t (role_perm_id, obj_id, ver_nbr, role_id, perm_id, actv_ind)
+INSERT INTO KRIM_ROLE_PERM_T (ROLE_PERM_ID, OBJ_ID, VER_NBR, ROLE_ID, PERM_ID, ACTV_IND)
   VALUES (
     'KR1003', uuid(), 1,
     (
       SELECT
-        role_id
-      FROM krim_role_t
-      WHERE role_nm = 'Initiator' AND nmspc_cd = 'KR-WKFLW'
+        ROLE_ID
+      FROM KRIM_ROLE_T
+      WHERE ROLE_NM = 'Initiator' AND NMSPC_CD = 'KR-WKFLW'
     ),
     (
       SELECT
-        perm_id
-      FROM krim_perm_t
-      WHERE nmspc_cd = 'KUALI' AND nm = 'Edit Kuali ENROUTE Document Route Status Code S'
+        PERM_ID
+      FROM KRIM_PERM_T
+      WHERE NMSPC_CD = 'KUALI' AND NM = 'Edit Kuali ENROUTE Document Route Status Code S'
     ),
     'Y'
   )
 ;
 ALTER TABLE KREN_NTFCTN_T ADD COLUMN DOC_TYP_NM VARCHAR(64)
 ;
-INSERT INTO krcr_cmpnt_t (nmspc_cd, cmpnt_cd, obj_id, ver_nbr, nm, actv_ind)
+INSERT INTO KRCR_CMPNT_T (NMSPC_CD, CMPNT_CD, OBJ_ID, VER_NBR, NM, ACTV_IND)
   VALUES ('KR-KRAD', 'Lookup', uuid(), 1, 'Lookup', 'Y')
 ;
 
@@ -201,22 +201,22 @@ INSERT INTO KRCR_PARM_T
   VALUES ('KR-KRAD', 'Lookup', 'RESULTS_LIMIT', uuid(), 1, 'CONFG', '200',
           'Maximum number of results returned in a look-up query.', 'A', 'KUALI')
 ;
-UPDATE krms_attr_defn_t SET nmspc_cd = 'KR-RULE' WHERE nm = 'peopleFlowName' AND nmspc_cd = 'KR_RULE'
+UPDATE KRMS_ATTR_DEFN_T SET NMSPC_CD = 'KR-RULE' WHERE NM = 'peopleFlowName' AND NMSPC_CD = 'KR_RULE'
 ;
-DELETE FROM krim_perm_attr_data_t
-WHERE attr_val = 'KRMS_TEST' AND perm_id =
+DELETE FROM KRIM_PERM_ATTR_DATA_T
+WHERE ATTR_VAL = 'KRMS_TEST' AND PERM_ID =
   (
     SELECT
-      perm_id
-    FROM krim_perm_t
-    WHERE nm = 'Maintain KRMS Agenda' AND nmspc_cd = 'KR-RULE-TEST'
+      PERM_ID
+    FROM KRIM_PERM_T
+    WHERE NM = 'Maintain KRMS Agenda' AND NMSPC_CD = 'KR-RULE-TEST'
   )
 ;
-UPDATE krim_perm_attr_data_t SET attr_val='RiceDocument'
-WHERE attr_val = '*' AND perm_id =
+UPDATE KRIM_PERM_ATTR_DATA_T SET ATTR_VAL='RiceDocument'
+WHERE ATTR_VAL = '*' AND PERM_ID =
   (
-    SELECT perm_id FROM krim_perm_t WHERE nm='Recall Document' AND nmspc_cd='KR-WKFLW'
+    SELECT PERM_ID FROM KRIM_PERM_T WHERE NM='Recall Document' AND nmspc_cd='KR-WKFLW'
   )
 ;
-ALTER TABLE krlc_cntry_t MODIFY COLUMN postal_cntry_nm VARCHAR(255) DEFAULT NULL
+ALTER TABLE KRLC_CNTRY_T MODIFY COLUMN POSTAL_CNTRY_NM VARCHAR(255) DEFAULT NULL
 ;
