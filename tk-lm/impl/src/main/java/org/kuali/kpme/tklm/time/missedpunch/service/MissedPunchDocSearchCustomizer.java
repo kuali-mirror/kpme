d3fcaba798977a580aac64a99ce9ad8f8d632cb6
jjhanso <jjhanso@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-09-23 01:57:15 +0000

    KPME-2817 - updating rice 2.3.1 release KPME-2829 - fixes for MissedPunch lookup
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5566 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/missedpunch/service/MissedPunchDocSearchCustomizer.java
@@ -0,0 +1,8 @@
+package org.kuali.kpme.tklm.time.missedpunch.service;
+
+
+import org.kuali.rice.kew.framework.document.search.DocumentSearchCustomizer;
+import org.kuali.rice.kns.workflow.attribute.DataDictionaryDocumentSearchCustomizer;
+
+public class MissedPunchDocSearchCustomizer extends DataDictionaryDocumentSearchCustomizer implements DocumentSearchCustomizer {
+}
