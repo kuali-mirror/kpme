e49ff3e981a6286e7fb51bc8f781e34c3349196b
neerajsk <neerajsk@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2013-12-13 20:14:26 +0000

    KPME-2368: Added a common KRAD maintenance controller for all HR BOs (that have a KRAD maintenance view defined, currently only about 7 BOs).  This controller intercepts user actions like 'save', 'route', 'approve' etc. to check for existence of newer versions of the BO. If a newer version exists, then the controller displays a KRAD dialog asking the user for confirmation to proceed with the particular action.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5931 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ core/impl/src/main/java/org/kuali/rice/krad/kpme/controller/EffectiveDateMaintenanceController.java
@@ -0,0 +1,157 @@
+package org.kuali.rice.krad.kpme.controller;
+
+import java.util.concurrent.Callable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kuali.kpme.core.api.bo.service.HrBusinessObjectService;
+import org.kuali.kpme.core.bo.HrBusinessObject;
+import org.kuali.kpme.core.service.HrServiceLocator;
+import org.kuali.rice.krad.maintenance.MaintenanceDocument;
+import org.kuali.rice.krad.web.controller.MaintenanceDocumentController;
+import org.kuali.rice.krad.web.form.DocumentFormBase;
+import org.kuali.rice.krad.web.form.MaintenanceDocumentForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping(value = "/kpme/effectiveDateMaintenance")
+public class EffectiveDateMaintenanceController extends MaintenanceDocumentController {
+	
+	private static final String KPME_EFFECTIVE_DATE_WARNING_DIALOG = "KPMEEffectiveDateWarning-Dialog";
+	private static final String HR_BUSINESS_OBJECT_SERVICE_NAME = "hrBusinessObjectService";
+
+
+	
+	@Override
+	@RequestMapping(params = "methodToCall=save")
+	public ModelAndView save(@ModelAttribute("KualiForm") final DocumentFormBase form, final BindingResult result, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+		return showEffectiveDateWarningIfNeeded(form, result, request, response, new Callable<ModelAndView>() {
+				@Override
+				public ModelAndView call() throws Exception {
+					// register the superclass method as the handler
+					return EffectiveDateMaintenanceController.super.save(form, result, request, response);
+				}
+		});        
+	}
+	
+	@Override
+	@RequestMapping(params = "methodToCall=route")
+	public ModelAndView route(@ModelAttribute("KualiForm") final DocumentFormBase form, final BindingResult result, final HttpServletRequest request, final HttpServletResponse response) {
+		// the try-catch below was needed since route was declared to throw no exceptions in the superclass.
+		try {
+			return showEffectiveDateWarningIfNeeded(form, result, request, response, new Callable<ModelAndView>() {
+					@Override
+					public ModelAndView call() throws Exception {
+						// register the superclass method as the handler
+						return EffectiveDateMaintenanceController.super.route(form, result, request, response);
+					}
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+			return getUIFModelAndView(form);
+		}
+	}
+	
+	@Override
+	@RequestMapping(params = "methodToCall=blanketApprove")
+	public ModelAndView blanketApprove(@ModelAttribute("KualiForm") final DocumentFormBase form, final BindingResult result, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+		return showEffectiveDateWarningIfNeeded(form, result, request, response, new Callable<ModelAndView>() {
+				@Override
+				public ModelAndView call() throws Exception {
+					// register the superclass method as the handler
+					return EffectiveDateMaintenanceController.super.blanketApprove(form, result, request, response);
+				}
+		});        
+	}
+	
+	@Override
+	@RequestMapping(params = "methodToCall=approve")
+	public ModelAndView approve(@ModelAttribute("KualiForm") final DocumentFormBase form, final BindingResult result, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+		return showEffectiveDateWarningIfNeeded(form, result, request, response, new Callable<ModelAndView>() {
+				@Override
+				public ModelAndView call() throws Exception {
+					// register the superclass method as the handler
+					return EffectiveDateMaintenanceController.super.approve(form, result, request, response);
+				}
+		});        
+	}
+	
+	@Override
+	@RequestMapping(params = "methodToCall=complete")
+	public ModelAndView complete(@ModelAttribute("KualiForm") final DocumentFormBase form, final BindingResult result, final HttpServletRequest request, final HttpServletResponse response) throws Exception {
+		return showEffectiveDateWarningIfNeeded(form, result, request, response, new Callable<ModelAndView>() {
+				@Override
+				public ModelAndView call() throws Exception {
+					// register the superclass method as the handler
+					return EffectiveDateMaintenanceController.super.complete(form, result, request, response);
+				}
+		});        
+	}
+	
+	
+	// This method containes the common pre-handler logic to show the effective date warning dialog (if needed) for any particular handler action that we wish to 'intercept'. 
+	// Currently this set of actions includes 'save', 'route' 'approve', 'complete' and 'blanket approve'. 
+	// The callable parameter encapsulates the actual super class handler method to be invoked in case of either an absence of warning or user's confirmation on the warning.
+	protected ModelAndView showEffectiveDateWarningIfNeeded(@ModelAttribute("KualiForm") DocumentFormBase form, 
+											BindingResult result, 
+											HttpServletRequest request, 
+											HttpServletResponse response, 
+											Callable<ModelAndView> callable) throws Exception {
+		
+		ModelAndView retVal = null;		
+				
+		// first check if the dialog has already been answered by the user; this is an optimization to avoid checking existence of newer BO version 
+		// in case its the second pass through (due to user input on the dialog shown in the first pass).
+		if (!hasDialogBeenAnswered(KPME_EFFECTIVE_DATE_WARNING_DIALOG, form)) {
+			// get the BO being maintained
+			HrBusinessObject newBo = this.getNewHrBusinessObjectInstance((MaintenanceDocumentForm) form);
+			// check if a newer version of this BO already exists in our DB
+			if (doesNewerVersionExist(newBo)) {
+				// redirect back to client to display lightbox
+				retVal = showDialog(KPME_EFFECTIVE_DATE_WARNING_DIALOG, form, request, response);
+			} 
+			else {
+				// just call the registered handler method; no warning is to be shown
+				retVal = callable.call();
+			}
+		} 
+		else {
+			// get the response entered by the user
+			boolean areYouSure = getBooleanDialogResponse(KPME_EFFECTIVE_DATE_WARNING_DIALOG, form, request, response);
+			// clear all dialogs in order to display warning again
+			form.getDialogManager().removeAllDialogs();
+			if (areYouSure) {
+				// call the registered handler method; user has confirmed
+				retVal = callable.call();
+			} 
+			else {
+				// do nothing, just show the form
+				retVal = getUIFModelAndView(form);
+			}
+		}
+        
+        return retVal;
+	}
+	
+	
+	protected HrBusinessObject getNewHrBusinessObjectInstance(MaintenanceDocumentForm form) {
+		MaintenanceDocument document = form.getDocument();
+		return (HrBusinessObject) document.getNewMaintainableObject().getDataObject();
+	}
+	
+	protected HrBusinessObjectService getHrBusinessObjectService() {
+		return HrServiceLocator.getService(HR_BUSINESS_OBJECT_SERVICE_NAME);		
+	}
+
+    protected boolean doesNewerVersionExist(HrBusinessObject newBo) {
+    	// use the hr BO service to check if a new version exists
+		return getHrBusinessObjectService().doesNewerVersionExist(newBo);
+    }
+	
+}
