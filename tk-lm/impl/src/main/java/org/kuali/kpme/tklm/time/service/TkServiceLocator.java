da97ae8572279455029f1dcfb2d8092b73baec50
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-01-01 05:16:25 +0000

    Automated license header update
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@5977 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/service/TkServiceLocator.java
+++ tk-lm/impl/src/main/java/org/kuali/kpme/tklm/time/service/TkServiceLocator.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2004-2013 The Kuali Foundation
+ * Copyright 2004-2014 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.kuali.kpme.tklm.time.service;
 //import org.kuali.hr.time.paytype.service.PayTypeService;
 //import org.kuali.hr.time.permission.service.TKPermissionService;
