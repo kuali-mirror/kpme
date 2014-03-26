cb49fa435b74592022dd49baa18901cafa48b634
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-03-26 04:04:41 +0000

    Automated license header update
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@6427 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- web/src/main/webapp/js/krad-ui.js
+++ web/src/main/webapp/js/krad-ui.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * This Javascript file added to resolve issue (KPME-3291) with Position Classification Maint doc and Inquiry due
  * to Navigation link : When one of the option like Funding, Qualification, Flags etc are disabled then
