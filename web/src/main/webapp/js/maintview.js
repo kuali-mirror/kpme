6334e315b4fb32f17412a4f17a7cba0ec9a7b535
jkakkad <jkakkad@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-04-14 09:04:27 +0000

    KPME-3369 In KRAD Maint docs, hitting enter or tab now it does not go to the lookup.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@6541 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- web/src/main/webapp/js/maintview.js
+++ web/src/main/webapp/js/maintview.js
@@ -17,3 +17,28 @@
 function showGrowl(message, title, theme) {
 	return;
 }
+
+jQuery(document).ready(function () {
+	jQuery("[title='Search Field']").attr("tabindex","-1");
+	jQuery("[name^='performLookup']").attr("tabindex","-1");
+	
+	jQuery(document).keypress(function(e) {
+		/* use IE naming first then firefox. */
+	    var elemType = event.srcElement ? event.srcElement.type : event.target.type;
+	    if (elemType != null && (elemType.toLowerCase() == 'textarea' || elemType.toLowerCase() == 'submit')) {
+	      // KULEDOCS-1728: textareas need to have the return key enabled
+	      return true;
+	    }
+		var initiator = event.srcElement ? event.srcElement.name : event.target.name;
+		var key = event.keyCode;
+		/* initiator is undefined check is to prevent return from doing anything if not in a form field since the initiator is undefined */
+		/* 13 is return key code */
+		/* length &gt; 0 check is to allow user to hit return on links */
+		if ( key == 13 ) {
+			if( initiator == undefined || ( initiator.length > 0) ) {
+			  // disallow enter key from fields that dont match prefix.
+			  return false;
+			}
+		}
+	});
+});
