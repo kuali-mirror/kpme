5f07b47a4631421ce83f1efa2a80ae876bd9a55d
umehta <umehta@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-05-05 12:32:00 +0000

    KPME-3417 : Created DepartmentAffiliationBoTest.
    git-svn-id: https://svn.kuali.org/repos/kpme/trunk@6686 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ core/impl/src/test/java/org/kuali/kpme/core/departmentAffiliation/DepartmentAffiliationBoTest.java
@@ -0,0 +1,46 @@
+package org.kuali.kpme.core.departmentAffiliation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+import org.kuali.kpme.core.api.departmentaffiliation.DepartmentAffiliation;
+import org.kuali.kpme.core.departmentaffiliation.DepartmentAffiliationBo;
+public class DepartmentAffiliationBoTest {
+
+	    private static Map<String, DepartmentAffiliation> testDepartmentAffiliationBos;
+	    public static DepartmentAffiliation.Builder deptAfflBuilder = DepartmentAffiliation.Builder.create();
+	    static {
+	    	
+	    	testDepartmentAffiliationBos = new HashMap<String, DepartmentAffiliation>();
+	        deptAfflBuilder.setDeptAfflType("TST-DEPTAFFL");
+	        deptAfflBuilder.setHrDeptAfflId("KPME-TEST-0001");
+	        deptAfflBuilder.setActive(true);
+	        
+	        deptAfflBuilder.setVersionNumber(1L);
+	        deptAfflBuilder.setObjectId("0804716a-cbb7-11e3-9cd3-51a754ad6a0a");
+	        deptAfflBuilder.setUserPrincipalId("admin");
+	        deptAfflBuilder.setId(deptAfflBuilder.getHrDeptAfflId());
+			deptAfflBuilder.setEffectiveLocalDate(new LocalDate(2012, 3, 1));
+			deptAfflBuilder.setCreateTime(DateTime.now());
+	        
+	        testDepartmentAffiliationBos.put(deptAfflBuilder.getDeptAfflType(), deptAfflBuilder.build());
+	        
+	    }
+
+	    @Test
+	    public void testNotEqualsWithGroup() {
+	        DepartmentAffiliation immutable = DepartmentAffiliationBoTest.getDepartmentAffiliation("TST-DEPTAFFL");
+	        DepartmentAffiliationBo bo = DepartmentAffiliationBo.from(immutable);
+	        Assert.assertFalse(bo.equals(immutable));
+	        Assert.assertFalse(immutable.equals(bo));
+	        Assert.assertEquals(immutable, DepartmentAffiliationBo.to(bo));
+	    }
+
+	    public static DepartmentAffiliation getDepartmentAffiliation(String deptAffiliation) {
+	        return testDepartmentAffiliationBos.get(deptAffiliation);
+	    }
+}
