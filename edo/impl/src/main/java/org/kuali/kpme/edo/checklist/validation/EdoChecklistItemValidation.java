80edf734de45d314ec7a96e860a597ff04dffed7
tkagata <tkagata@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-06-20 21:36:00 +0000

    git-svn-id: https://svn.kuali.org/repos/kpme/branches/kpme-2.2@7017 6c5f4195-72ea-6250-e7e4-e498f552a5df

---
+++ edo/impl/src/main/java/org/kuali/kpme/edo/checklist/validation/EdoChecklistItemValidation.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.edo.checklist.validation;
+
+import java.util.List;
+
+import org.kuali.kpme.edo.api.checklist.EdoChecklistItem;
+import org.kuali.kpme.edo.api.checklist.EdoChecklistSection;
+import org.kuali.kpme.edo.checklist.EdoChecklistItemBo;
+import org.kuali.kpme.edo.service.EdoServiceLocator;
+import org.kuali.rice.krad.maintenance.MaintenanceDocument;
+import org.kuali.rice.krad.rules.MaintenanceDocumentRuleBase;
+
+public class EdoChecklistItemValidation extends MaintenanceDocumentRuleBase {
+
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+		
+		boolean isValid = super.processCustomRouteDocumentBusinessRules(document);
+		LOG.debug("entering custom validation for edo checklist item");
+		
+		EdoChecklistItemBo checklistItem = (EdoChecklistItemBo) this.getNewDataObject();
+		
+		if (checklistItem != null) {
+			isValid &= validateSectionID(checklistItem);
+			isValid &= validateChecklistOrdinal(checklistItem);
+		}
+		return isValid;
+	}
+	
+	private boolean validateSectionID(EdoChecklistItemBo checklistItem) {
+		
+		EdoChecklistSection aSection = EdoServiceLocator.getChecklistSectionService().getChecklistSectionByID(checklistItem.getChecklistSectionID()) ;
+		String errorMes = "Checklist Section '"+ checklistItem.getChecklistSectionID() + "'";
+		if(aSection == null) {
+			this.putFieldError("dataObject.edoChecklistSection", "error.existence", errorMes);
+			return false;
+		} 
+		
+		return true;
+	}
+	
+	private boolean validateChecklistOrdinal(EdoChecklistItemBo checklistItem) {
+
+		List<EdoChecklistItem> listItems = EdoServiceLocator.getChecklistItemService().getChecklistItemsBySectionID(checklistItem.getChecklistSectionID(), checklistItem.getEffectiveLocalDate());
+		for (EdoChecklistItem listItem : listItems) {
+			if (listItem.getChecklistItemOrdinal() == checklistItem.getChecklistItemOrdinal()) {
+				// error.checklist.ordinal.exist ={0} '{1}' is already in use.
+				String[] params = new String[2];
+				params[0] = "Checklist Item Ordenal";
+				params[1] = checklistItem.getChecklistItemOrdinal()+"";
+				this.putFieldError("dataObject.edoChecklistItem", "error.checklist.ordinal.exist", params);
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+}
---
+++ edo/impl/src/main/java/org/kuali/kpme/edo/checklist/validation/EdoChecklistSectionValidation.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2004-2014 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl2.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kpme.edo.checklist.validation;
+
+import java.util.List;
+
+import org.kuali.kpme.edo.api.checklist.EdoChecklist;
+import org.kuali.kpme.edo.api.checklist.EdoChecklistItem;
+import org.kuali.kpme.edo.api.checklist.EdoChecklistSection;
+import org.kuali.kpme.edo.checklist.EdoChecklistItemBo;
+import org.kuali.kpme.edo.checklist.EdoChecklistSectionBo;
+import org.kuali.kpme.edo.service.EdoServiceLocator;
+import org.kuali.rice.krad.maintenance.MaintenanceDocument;
+import org.kuali.rice.krad.rules.MaintenanceDocumentRuleBase;
+
+public class EdoChecklistSectionValidation extends MaintenanceDocumentRuleBase {
+
+	@Override
+	protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+		
+		boolean isValid = super.processCustomRouteDocumentBusinessRules(document);
+		LOG.debug("entering custom validation for edo checklist section");
+		
+		EdoChecklistSectionBo checklistSection = (EdoChecklistSectionBo) this.getNewDataObject();
+		
+		if (checklistSection != null) {
+			isValid &= validateChecklistID(checklistSection);
+			isValid &= validateSectionOrdinal(checklistSection);
+		}
+		return isValid;
+	}
+	
+	private boolean validateChecklistID(EdoChecklistSectionBo checklistSection) {
+
+		System.out.println("checklist id "+checklistSection.getChecklistID());
+		EdoChecklist aSection = EdoServiceLocator.getChecklistService().getChecklistByID(checklistSection.getChecklistID()) ;
+		String errorMes = "Checklist '"+ checklistSection.getChecklistID() + "'";
+		if(aSection == null) {
+			System.out.println("check list doesn't exist");
+			this.putFieldError("dataObject.edoChecklist", "error.existence", errorMes);
+			return false;
+		} else {
+			System.out.println("check list exists "+aSection);
+		}
+		
+		return true;
+	}
+	
+	private boolean validateSectionOrdinal(EdoChecklistSectionBo checklistSection) {
+
+		List<EdoChecklistSection> sections = EdoServiceLocator.getChecklistSectionService().getChecklistSectionsByChecklistID(checklistSection.getChecklistID(), checklistSection.getEffectiveLocalDate());
+		for (EdoChecklistSection section : sections) {
+			if (checklistSection.getChecklistSectionOrdinal() == section.getChecklistSectionOrdinal()) {
+				// error.checklist.ordinal.exist ={0} '{1}' is already in use.
+				String[] params = new String[2];
+				params[0] = "Checklist Section Ordenal";
+				params[1] = checklistSection.getChecklistSectionOrdinal()+"";
+				this.putFieldError("dataObject.edoChecklistSection", "error.checklist.ordinal.exist", params);
+				return false;
+			}
+		}
+		
+		return true;
+		
+	}
+}
