bbd4ce4d54f125cfbcb2148826547527a2ad3153
xichen <xichen@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-07-08 16:29:58 +0000

    KPME-3611
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/kpme-2.2@7121 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- edo/impl/src/main/java/org/kuali/kpme/edo/util/EdoContext.java
+++ edo/impl/src/main/java/org/kuali/kpme/edo/util/EdoContext.java
@@ -368,7 +368,7 @@ public class EdoContext {
         if (selectedCandidate != null) {
             DossierProcessDocumentHeader documentHeader = EdoServiceLocator.getDossierProcessDocumentHeaderService().getDossierProcessDocumentHeader(selectedCandidate.getCandidateDossierID().intValue());
             if(documentHeader != null) {
-                List<DossierProcessDocumentHeader> suppDocHeaders = EdoServiceLocator.getDossierProcessDocumentHeaderService().getPendingSupplementalDocuments(documentHeader.getDossierId());
+                List<DossierProcessDocumentHeader> suppDocHeaders = EdoServiceLocator.getDossierProcessDocumentHeaderService().getPendingSupplementalDocuments(new Integer(documentHeader.getEdoDossierId()));
                 if (CollectionUtils.isNotEmpty(suppDocHeaders)) {
                     boolean isWaiting = false;
                     for(DossierProcessDocumentHeader docHeader : suppDocHeaders) {
