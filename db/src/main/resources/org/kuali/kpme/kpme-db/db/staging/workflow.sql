3de65ff4fc4aa2febe859572c280f922e8a7a69a
jcaddel <jcaddel@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-08-05 05:35:45 +0000

    Automated license header update
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/kpme-2.1@7304 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- db/src/main/resources/org/kuali/kpme/kpme-db/db/staging/workflow.sql
+++ db/src/main/resources/org/kuali/kpme/kpme-db/db/staging/workflow.sql
@@ -1,3 +1,19 @@
+--
+-- Copyright 2004-2014 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
 
 INSERT INTO KREW_RULE_ATTR_T (RULE_ATTR_ID,NM,LBL,RULE_ATTR_TYP_CD,CLS_NM,APPL_ID,DESC_TXT,XML,OBJ_ID,VER_NBR) VALUES ('1645','BalanceTransferRoleAttribute','BalanceTransferRoleAttribute','RuleAttribute','org.kuali.kpme.tklm.leave.workflow.BalanceTransferRoleAttribute','KPME','BalanceTransferRoleAttribute',null,'445eaadd-be2e-4047-981d-35062db63034',1)
 ;
