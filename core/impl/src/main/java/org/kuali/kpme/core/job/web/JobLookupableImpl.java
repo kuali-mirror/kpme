50680d3ddd8cad94451530d7a39210d3781c123b
mlemons <mlemons@6c5f4195-72ea-6250-e7e4-e498f552a5df>
2014-08-12 15:25:16 +0000

    KPME-3747 : fixing lookups by location, using RICE search helper
    git-svn-id: https://svn.kuali.org/repos/kpme/branches/kpme-2.1@7330 6c5f4195-72ea-6250-e7e4-e498f552a5df

--- core/impl/src/main/java/org/kuali/kpme/core/job/web/JobLookupableImpl.java
+++ core/impl/src/main/java/org/kuali/kpme/core/job/web/JobLookupableImpl.java
@@ -15,43 +15,208 @@
  */
 package org.kuali.kpme.core.job.web;
 
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kpme.core.api.department.Department;
 import org.kuali.kpme.core.api.job.Job;
+import org.kuali.kpme.core.api.namespace.KPMENamespace;
+import org.kuali.kpme.core.api.permission.KPMEPermissionTemplate;
 import org.kuali.kpme.core.job.JobBo;
 import org.kuali.kpme.core.lookup.KPMELookupableImpl;
+import org.kuali.kpme.core.lookup.KpmeHrGroupKeyedBusinessObjectLookupableImpl;
+import org.kuali.kpme.core.role.KPMERoleMemberAttribute;
 import org.kuali.kpme.core.service.HrServiceLocator;
 import org.kuali.kpme.core.util.TKUtils;
 import org.kuali.rice.core.api.mo.ModelObjectUtils;
+import org.kuali.rice.kim.api.KimConstants;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.kim.api.services.KimApiServiceLocator;
 import org.kuali.rice.krad.bo.BusinessObject;
+import org.kuali.rice.krad.lookup.LookupUtils;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.form.LookupForm;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class JobLookupableImpl extends KPMELookupableImpl{
+//public class JobLookupableImpl extends KPMELookupableImpl{
+public class JobLookupableImpl extends KpmeHrGroupKeyedBusinessObjectLookupableImpl {
     private static final ModelObjectUtils.Transformer<Job, JobBo> toJobBo =
             new ModelObjectUtils.Transformer<Job, JobBo>() {
                 public JobBo transform(Job input) {
                     return JobBo.from(input);
                 };
             };
+/*
+	@SuppressWarnings("unchecked")
+	@Override
+	protected List<?> getSearchResults(LookupForm form, Map<String, String> searchCriteria, boolean unbounded) {
+        String userPrincipalId = GlobalVariables.getUserSession().getPrincipalId();
 
+        Integer searchResultsLimit = null;
+
+        Collection<?> rawSearchResults;
+
+        // removed blank search values and decrypt any encrypted search values
+        Map<String, String> nonBlankSearchCriteria = processSearchCriteria(form, searchCriteria);
+
+        if (nonBlankSearchCriteria == null) {
+            return new ArrayList<Object>();
+        }
+
+        if (!unbounded) {
+            searchResultsLimit = LookupUtils.getSearchResultsLimit(getDataObjectClass(), form);
+        }
+
+        rawSearchResults = getLookupService().findCollectionBySearchHelper(getDataObjectClass(),
+                nonBlankSearchCriteria, unbounded, searchResultsLimit);
+
+        if (rawSearchResults == null) {
+            rawSearchResults = new ArrayList<Object>();
+        } else {
+            sortSearchResults(form, (List<?>) rawSearchResults);
+        }
+
+        List<AssignmentBo> filteredResults = filterLookupAssignments((List<AssignmentBo>)rawSearchResults, userPrincipalId);
+
+        generateLookupResultsMessages(form, nonBlankSearchCriteria, filteredResults, unbounded);
+
+        return filteredResults;
+	}
+ */
+//    protected List<AssignmentBo> filterLookupAssignments(List<AssignmentBo> rawResults, String userPrincipalId) {
+
+/*
     @Override
-    public List<? extends BusinessObject> getSearchResults(LookupForm form, Map<String, String> searchCriteria, boolean unbounded) {
-        String principalId = searchCriteria.get("principalId");
-        String firstName = searchCriteria.get("firstName");
-        String lastName = searchCriteria.get("lastName");
-        String jobNumber = searchCriteria.get("jobNumber");
-        String dept = searchCriteria.get("dept");
-        String positionNumber = searchCriteria.get("positionNumber");
-        String hrPayType = searchCriteria.get("hrPayType");
-        String fromEffdt = TKUtils.getFromDateString(searchCriteria.get("effectiveDate"));
-        String toEffdt = TKUtils.getToDateString(searchCriteria.get("effectiveDate"));
-        String active = searchCriteria.get("active");
-        String showHist = searchCriteria.get("history");
+    public List<Job> getJobs(String userPrincipalId, String principalId, String firstName, String lastName, String jobNumber,
+                             String dept, String positionNbr, String payType,
+                             LocalDate fromEffdt, LocalDate toEffdt, String active, String showHistory) {
+    	List<JobBo> results = new ArrayList<JobBo>();
 
-        return ModelObjectUtils.transform(HrServiceLocator.getJobService().getJobs(GlobalVariables.getUserSession().getPrincipalId(), principalId, firstName, lastName, jobNumber, dept, positionNumber, hrPayType,
-                TKUtils.formatDateString(fromEffdt), TKUtils.formatDateString(toEffdt), active, showHist), toJobBo);
+    	List<JobBo> jobObjs = new ArrayList<JobBo>();
+
+        if (StringUtils.isNotEmpty(firstName) || StringUtils.isNotEmpty(lastName)) {
+            Map<String, String> fields = new HashMap<String, String>();
+            fields.put("firstName", firstName);
+            fields.put("lastName", lastName);
+            List<Person> people = KimApiServiceLocator.getPersonService().findPeople(fields);
+
+            for (Person p : people) {
+                List<JobBo> jobsForPerson = jobDao.getJobs(p.getPrincipalId(), jobNumber, dept, positionNbr, payType, fromEffdt, toEffdt, active, showHistory);
+                jobObjs.addAll(jobsForPerson);
+            }
+        } else {
+        	jobObjs.addAll(jobDao.getJobs(principalId, jobNumber, dept, positionNbr, payType, fromEffdt, toEffdt, active, showHistory));
+        }
+
+    	for (JobBo jobObj : jobObjs) {
+        	String department = jobObj.getDept();
+        	Department departmentObj = HrServiceLocator.getDepartmentService().getDepartment(department, jobObj.getGroupKeyCode(), jobObj.getEffectiveLocalDate());
+            String location = departmentObj != null ? departmentObj.getGroupKey().getLocationId() : null;
+
+        	Map<String, String> roleQualification = new HashMap<String, String>();
+        	roleQualification.put(KimConstants.AttributeConstants.PRINCIPAL_ID, userPrincipalId);
+            roleQualification.put(KPMERoleMemberAttribute.DEPARTMENT.getRoleMemberAttributeName(), department);
+            roleQualification.put(KPMERoleMemberAttribute.GROUP_KEY_CODE.getRoleMemberAttributeName(), jobObj.getGroupKeyCode());
+        	roleQualification.put(KPMERoleMemberAttribute.LOCATION.getRoleMemberAttributeName(), location);
+
+        	if (!KimApiServiceLocator.getPermissionService().isPermissionDefinedByTemplate(KPMENamespace.KPME_WKFLW.getNamespaceCode(),
+    				KPMEPermissionTemplate.VIEW_KPME_RECORD.getPermissionTemplateName(), new HashMap<String, String>())
+    		  || KimApiServiceLocator.getPermissionService().isAuthorizedByTemplate(userPrincipalId, KPMENamespace.KPME_WKFLW.getNamespaceCode(),
+    				  KPMEPermissionTemplate.VIEW_KPME_RECORD.getPermissionTemplateName(), new HashMap<String, String>(), roleQualification)) {
+        		results.add(jobObj);
+        	}
+    	}
+
+    	return toImmutable(results);
+    }
+ */
+
+
+
+    protected List<JobBo> filterLookupJobs(List<JobBo> rawResults, String userPrincipalId)
+    {
+        List<JobBo> results = new ArrayList<JobBo>();
+        for (JobBo jobObj : rawResults)
+        {
+            String department = jobObj.getDept();
+            Department departmentObj = HrServiceLocator.getDepartmentService().getDepartment(department, jobObj.getGroupKeyCode(), jobObj.getEffectiveLocalDate());
+            String location = departmentObj != null ? departmentObj.getGroupKey().getLocationId() : null;
+
+            Map<String, String> roleQualification = new HashMap<String, String>();
+            roleQualification.put(KimConstants.AttributeConstants.PRINCIPAL_ID, userPrincipalId);
+            roleQualification.put(KPMERoleMemberAttribute.DEPARTMENT.getRoleMemberAttributeName(), department);
+            roleQualification.put(KPMERoleMemberAttribute.GROUP_KEY_CODE.getRoleMemberAttributeName(), jobObj.getGroupKeyCode());
+            roleQualification.put(KPMERoleMemberAttribute.LOCATION.getRoleMemberAttributeName(), location);
+
+            if (!KimApiServiceLocator.getPermissionService().isPermissionDefinedByTemplate(KPMENamespace.KPME_WKFLW.getNamespaceCode(),
+                    KPMEPermissionTemplate.VIEW_KPME_RECORD.getPermissionTemplateName(), new HashMap<String, String>())
+                    || KimApiServiceLocator.getPermissionService().isAuthorizedByTemplate(userPrincipalId, KPMENamespace.KPME_WKFLW.getNamespaceCode(),
+                    KPMEPermissionTemplate.VIEW_KPME_RECORD.getPermissionTemplateName(), new HashMap<String, String>(), roleQualification)) {
+                results.add(jobObj);
+            }
+        }
+        return results;
+    }
+
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<?> getSearchResults(LookupForm form, Map<String, String> searchCriteria, boolean unbounded) {
+        String userPrincipalId = GlobalVariables.getUserSession().getPrincipalId();
+
+        Integer searchResultsLimit = null;
+
+        List<JobBo> rawSearchResults = new ArrayList<JobBo>();
+
+        // removed blank search values and decrypt any encrypted search values
+        Map<String, String> nonBlankSearchCriteria = processSearchCriteria(form, searchCriteria);
+        nonBlankSearchCriteria.remove("firstName");
+        nonBlankSearchCriteria.remove("lastName");
+
+
+        if (nonBlankSearchCriteria == null) {
+            return new ArrayList<Object>();
+        }
+
+        if (!unbounded) {
+            searchResultsLimit = LookupUtils.getSearchResultsLimit(getDataObjectClass(), form);
+        }
+
+        if (StringUtils.isNotEmpty(searchCriteria.get("firstName")) || StringUtils.isNotEmpty(searchCriteria.get("lastName"))) {
+            Map<String, String> fields = new HashMap<String, String>();
+            fields.put("firstName", searchCriteria.get("firstName"));
+            fields.put("lastName", searchCriteria.get("lastName"));
+            List<Person> people = KimApiServiceLocator.getPersonService().findPeople(fields);
+
+
+            for (Person p : people) {
+                Map<String, String> personSearch = new HashMap<String, String>();
+                personSearch.putAll(nonBlankSearchCriteria);
+                personSearch.put("principalId", p.getPrincipalId());
+
+                List<JobBo> res = (List<JobBo>)getLookupService().findCollectionBySearchHelper(getDataObjectClass(),
+                        personSearch, unbounded, searchResultsLimit);
+
+                rawSearchResults.addAll(res);
+            }
+        }
+        else
+        {
+            rawSearchResults = (List<JobBo>)getLookupService().findCollectionBySearchHelper(getDataObjectClass(),
+                    nonBlankSearchCriteria, unbounded, searchResultsLimit);
+        }
+
+        if (rawSearchResults == null) {
+            rawSearchResults = new ArrayList<JobBo>();
+        } else {
+            sortSearchResults(form, (List<?>) rawSearchResults);
+        }
+
+
+        List<JobBo> filteredResults = filterLookupJobs(rawSearchResults, userPrincipalId);
+
+        generateLookupResultsMessages(form, nonBlankSearchCriteria, filteredResults, unbounded);
+
+        return filteredResults;
     }
 
 
